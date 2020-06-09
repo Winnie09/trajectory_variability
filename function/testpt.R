83df34497b5738990b3221554582a86743704580
Wenpin Hou <wp.hou3@gmail.com>
2020-06-08 23:06:16 -0400

    edit testtime testvar

--- function/testpt.R
+++ function/testpt.R
@@ -1,5 +1,10 @@
 testpt <- function(expr, cellanno, pseudotime, design, permuiter=10, EMmaxiter=100, EMitercutoff=1, verbose=F, ncores=detectCores(), type = 'Variable') {
   ## test.type = c('Variable', 'Time')
+  if (ncol(pseudotime) > 1) {
+    pseudotime <- pseudotime[order(pseudotime[,2]), ]
+  } else {
+    print('pseudotime should be a dataframe containing 1st column cell and 2nd column pseudotime...')
+  }
   if (type == 'Variable'){
     print('Testing on variable ...')
     return(testpt_Variable(expr=expr, cellanno=cellanno, pseudotime=pseudotime, design=design, permuiter=permuiter, EMmaxiter=EMmaxiter, EMitercutoff=EMitercutoff, verbose=verbose, ncores=ncores))
@@ -8,4 +13,5 @@ testpt <- function(expr, cellanno, pseudotime, design, permuiter=10, EMmaxiter=1
     return(testpt_Time(expr=expr, cellanno=cellanno, pseudotime=pseudotime, design=design, permuiter=permuiter, EMmaxiter=EMmaxiter, EMitercutoff=EMitercutoff, verbose=verbose, ncores=ncores))
   }
 }
+
 
