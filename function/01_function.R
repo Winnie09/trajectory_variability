ddd5c5661f4efcd19af844200b318760a5480795
Wenpin Hou <wp.hou3@gmail.com>
2020-09-27 14:14:57 -0400

    update

--- function/01_function.R
+++ function/01_function.R
@@ -1,9 +1,8 @@
-allfiles <- list.files('/dcl02/hongkai/data/whou/trajectory_variability/function')
+allfiles <- list.files('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/function/')
 allfiles = allfiles[!grepl('01_function.R',allfiles)]
 allfiles <- allfiles[grepl('.R$', allfiles)]
 for (f in allfiles){
-  source(paste0('/dcl02/hongkai/data/whou/trajectory_variability/function/', f))
+  source(paste0('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/function/', f))
 }
 rm(allfiles)
-
 
