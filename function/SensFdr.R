8ecdf7370fffb1f92d8f86c2734e67e572b5ef04
Wenpin Hou <wp.hou3@gmail.com>
2020-09-29 08:13:19 -0400

    update SensFdr

--- function/SensFdr.R
+++ function/SensFdr.R
@@ -1,10 +1,11 @@
 SensFdr <- function(TruePositive, Statistics){
   ## Statistics: a dataframe or matrix, should contain a column of fdr , for example c('FStat','P.Value','adj.P.Val')
   ## find the column of fdr
+  Statistics <- Statistics[, -which(colSums(is.na(Statistics)) == nrow(Statistics))]
   fdrchar <- intersect(colnames(Statistics), c('adj.P.Val','adj.pvalue','fdr','FDR','Fdr','adj.p', 'adj.P','adj.Pval'))
   fdrcol <- which(colnames(Statistics) == fdrchar)
   ## if not ordered by significance, then rank by significance
-  Statistics = Statistics[complete.cases(Statistics), , drop = FALSE]
+
   if (sum((diff(Statistics[, fdrcol])<0)+0) > 0){
     Statistics <- Statistics[order(Statistics[,fdrcol]), , drop = FALSE]
   }
@@ -23,4 +24,5 @@ SensFdr <- function(TruePositive, Statistics){
   colnames(perf) <- c('Sensitivity','Real_FDR','Reported_FDR')
   rbind(c(0,0,0),perf)
 }
+
 
