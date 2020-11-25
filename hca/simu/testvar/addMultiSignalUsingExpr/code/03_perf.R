6baadbb5f6dbe9166f05f9ccca5902badabe12c4
Wenpin Hou <wp.hou3@gmail.com>
2020-11-24 21:46:04 -0500

    add changes

--- hca/simu/testvar/addMultiSignalUsingExpr/code/03_perf.R
+++ hca/simu/testvar/addMultiSignalUsingExpr/code/03_perf.R
@@ -1,5 +1,6 @@
-ddir <- '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/simu/testvar/addMultiSignalUsingExpr/result/'
-rdir <- '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/simu/testvar/addMultiSignalUsingExpr/perf/'
+library(here)
+ddir <- paste0(here('hca','simu', 'testvar', 'addMultiSignalUsingExpr', 'result'), '/')
+rdir <- paste0(here('hca','simu', 'testvar', 'addMultiSignalUsingExpr', 'perf'), '/')
 
 selgene <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/data/simu/testvar/addMultiSignalUsingExpr/selgene/selgene.rds')
 source('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/function/01_function.R')
@@ -25,6 +26,47 @@ df2 <- sapply(af, function(f){
   c(sub('.rds','',f), 'EM_meandiff', AreaUnderSensFdr(SensFdr(selgene, res)))
 })
 df2 = t(df2)
+
+
+## merge trend diff and mean diff, get the minimum fdr as the fdr for each gene
+af = list.files(paste0(ddir, 'EM/'))
+df.merge <- sapply(af, function(f){
+  print(f)
+  r = readRDS(paste0(ddir, 'EM/', f))
+  res1 = data.frame(adj.P.Val = r[['fdr']], foldchange = r[['foldchange']], stringsAsFactors = F)
+  r = readRDS(paste0(ddir, 'EM_meandiff/', f))
+  res2 = r[,c('adj.P.Val', 'logFC')]
+  colnames(res2) <- colnames(res1)
+  
+  res <- lapply(1:nrow(res1), function(i){
+    if (res1[i,1] == res2[i,1] & res1[i,2] > res2[i,2]) {
+      res1[i,]
+    } else if (res1[i,1] < res2[i,1]){
+      res1[i,]
+    }
+    else {
+      res2[i,]
+    }
+  })
+  res <- do.call(rbind, res)
+  res = res[order(res[,1], -res[,2]), ]
+  c(sub('.rds','',f), 'EM_merge_min', AreaUnderSensFdr(SensFdr(selgene, res)))
+})
+df.merge.min = t(df.merge)
+
+## merge trend diff and mean diff, 1-(1-fdr1)(1-fdr2): under null,  the probability of oberving the data or more extreme cases in at least one situation
+af = list.files(paste0(ddir, 'EM/'))
+df.merge <- sapply(af, function(f){
+  print(f)
+  r1 = readRDS(paste0(ddir, 'EM/', f))
+  
+  r2 = readRDS(paste0(ddir, 'EM_meandiff/', f))
+  res <- data.frame(fdr = 1 - (1 - r1[['fdr']]) * (1 - r2[,'adj.P.Val']), stringsAsFactors = FALSE)
+  rownames(res) <- names(r1[['fdr']])
+  res = res[order(res[,1]), ,drop = FALSE]
+  c(sub('.rds','',f), 'EM_merge_atleast', AreaUnderSensFdr(SensFdr(selgene, res)))
+})
+df.merge.atleast = t(df.merge)
 
 # ------------------
 # other methods 
@@ -82,6 +124,13 @@ df6 = t(df6)
 
 res <- rbind(df1, df2, df3, df4, df5, df6)
 colnames(res) <- c('Signal.Strength', 'Method', 'Fdr.Diff', 'AUC')
-saveRDS(res, paste0(rdir, 'perf.rds'))
+
+## add EM merge results
+a = rbind(df.merge.min, df.merge.atleast)
+b = data.frame(a[,1], a[,2], a[,3], a[,4], stringsAsFactors = F)
+colnames(b) <- colnames(res)
+res2 = rbind(res,b)
+saveRDS(res2, paste0(rdir, 'perf.rds'))
+
 
 
--- hca/simu/testvar/addMultiSignalUsingExpr/code/04_plot_perf.R
+++ hca/simu/testvar/addMultiSignalUsingExpr/code/04_plot_perf.R
@@ -8,20 +8,20 @@ pd[,3] = as.numeric(pd[,3])
 pd[,4] = as.numeric(pd[,4])
 library(ggplot2)
 library(gridExtra)
-pdf(paste0(pdir, 'fdr_diff_auc.pdf'), width =11, height = 3.6)
+library(RColorBrewer)
+pdf(paste0(pdir, 'fdr_diff_auc.pdf'), width =11, height = 3.2)
 p1 <- ggplot(pd, aes(x = SignalStrength, y = Fdr.Diff, color=Method)) + 
   geom_point(size=1.5)  + 
   geom_line(size=0.2) + 
   theme_classic() + 
   ylab('fdr.diff(real~reported - 0.25*0.25/2)') +
-  scale_color_brewer(palette = 'Dark2') 
+  scale_color_manual(values = colorRampPalette(brewer.pal(8,'Set1'))(length(unique(pd$Method))))
 p2 <- ggplot(pd, aes(x = SignalStrength, y = AUC, color=Method)) + 
   geom_point(size=1.5)  + 
   geom_line(size=0.2) + 
   theme_classic() + 
   ylab('Area Under Sensitivity Real_FDR curve') +
-  scale_color_brewer(palette = 'Dark2') 
+  scale_color_manual(values = colorRampPalette(brewer.pal(8,'Set1'))(length(unique(pd$Method))))
 gridExtra::grid.arrange(p1, p2, nrow = 1)
 dev.off()
-
 
