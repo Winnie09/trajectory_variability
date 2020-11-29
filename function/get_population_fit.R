0793ed761ef0f8acb71bb38d764f4928a851af7c
Wenpin Hou <wp.hou3@gmail.com>
2020-11-29 10:58:27 -0500

    add mySTIP

--- function/get_population_fit.R
+++ function/get_population_fit.R
@@ -1,39 +1,54 @@
-get_population_fit <- function(testobj, variable = 'condition', value = NULL, gene){
+get_population_fit <- function(testobj,
+                               variable = 'condition',
+                               value = NULL,
+                               gene) {
+  ## if variable = NA, then return population fit for test on time
   design = testobj$design
   pseudotime = testobj$pseudotime
   knotnum = testobj$knotnum
   pseudotime = pseudotime[order(pseudotime)]
   
-  beta <- lapply(testobj$parameter[gene], function(i){
+  beta <- lapply(testobj$parameter[gene], function(i) {
     i$beta
   })
   names(beta) <- gene
-  if (sum(design[,1])!=nrow(design)) print("The first column of design matrix should be all 1s (intercept)!")
+  if (sum(design[, 1]) != nrow(design))
+    print("The first column of design matrix should be all 1s (intercept)!")
   colnames(design)[1] <- 'intercept'
-  design <- unique(design[,c('intercept', variable)])
-
-  rownames(design) <- paste0(variable, '_', unique(design[, variable]))
-
-  x <- sapply(row.names(design),function(i) {
-        kronecker(diag(knotnum[[gene]] + 4), design[i,])
-      },simplify = F)
+  if (is.na(variable)) {
+    design = design[, 1, drop = FALSE]
+  } else {
+    design <- unique(design[, c('intercept', variable)])
+    rownames(design) <-
+      paste0(variable, '_', unique(design[, variable]))
+  }
+  
+  
+  x <- sapply(row.names(design), function(i) {
+    kronecker(diag(knotnum[[gene]] + 4), design[i, , drop = FALSE])
+  }, simplify = FALSE)
   maxknotallowed <- 30
-  philist <- lapply(0:maxknotallowed,function(num.knot) {
-    if (num.knot==0) {
-      phi <- cbind(1,bs(pseudotime))
+  philist <- lapply(0:maxknotallowed, function(num.knot) {
+    if (num.knot == 0) {
+      phi <- cbind(1, bs(pseudotime))
     } else {
-      knots = seq(min(pseudotime),max(pseudotime),length.out=num.knot+2)[2:(num.knot+1)]
-      phi <- cbind(1,bs(pseudotime,knots = knots))
+      knots = seq(min(pseudotime), max(pseudotime), length.out = num.knot + 2)[2:(num.knot +
+                                                                                    1)]
+      phi <- cbind(1, bs(pseudotime, knots = knots))
     }
   })
   names(philist) <- as.character(0:maxknotallowed)
-
+  
   phi <- philist[[as.character(knotnum[gene])]]
   beta <- beta[[gene]]
-
-  fit <- lapply(x, function(i){
-    phi %*% t(i) %*% beta
-  })
-  names(fit) <- names(x)
+  
+  if (is.na(variable)) {
+    fit <- t(phi %*% t(x[[1]]) %*% beta)[1,]
+  } else {
+    fit <- lapply(x, function(i) {
+      phi %*% t(i) %*% beta
+    })
+    names(fit) <- names(x)
+  }
   return(fit)
 }
---
+++ function/mySTIP.R
@@ -0,0 +1,101 @@
+mySTIP <- function(fit, gl) {
+  ## fit: gene by cell matrix. colnames: pseudotime of the cells. rownames: gene names.
+  ## gl: a vector of genes (should be within the rownames) to be highlighted by texts.
+  dn <- dimnames(fit)
+  fit <- t(apply(fit, 1, scale))
+  dimnames(fit) <- dn
+  gene <- row.names(fit)
+  
+  zpdirection <- fit[, 1] < fit[, ncol(fit)]
+  
+  zp <- apply(fit, 1, function(sf) {
+    names(which(sapply(1:(length(
+      sf
+    ) - 1), function(i)
+      sf[i] * sf[i + 1] < 0)))
+  })
+  zpnum <- sapply(zp, length)
+  inczp <- names(which(zpdirection[zpnum == 1]))
+  deczp <- names(which(!zpdirection[zpnum == 1]))
+  multipoint <- names(zpnum)[zpnum > 1]
+  m1 <- names(which(fit[multipoint, 1] > 0))
+  m2 <- names(which(fit[multipoint, 1] < 0))
+  
+  geneorder <- NULL
+  
+  if (length(deczp) > 0) {
+    tmp <- unlist(zp[deczp])
+    n <- names(tmp)
+    tmp <- as.numeric(tmp)
+    names(tmp) <- n
+    geneorder <-
+      c(geneorder, names(sort(tmp, decreasing = F)))
+  }
+  geneorder <-
+    c(geneorder, names(sort(sapply(zp[m2], function(i)
+      as.numeric(i[1])))))
+  if (length(inczp) > 0) {
+    tmp <- unlist(zp[inczp])
+    n <- names(tmp)
+    tmp <- as.numeric(tmp)
+    names(tmp) <- n
+    geneorder <- c(geneorder, names(sort(tmp)))
+  }
+  geneorder <-
+    c(geneorder, names(sort(sapply(zp[m1], function(i)
+      as.numeric(i[1])))))
+  geneorder <- rev(geneorder)
+  plotdata <- fit[geneorder, ]
+  plotdata <- melt(plotdata)
+  colnames(plotdata) <- c("Gene", "Pseudotime", "Expression")
+  
+  p1 <-
+    ggplot(plotdata, aes(Pseudotime, Gene, fill = Expression)) + geom_tile() + theme_classic() + scale_fill_gradient2(low =
+                                                                                                                        "blue",
+                                                                                                                      high = "red",
+                                                                                                                      midpoint = 0) + theme(
+                                                                                                                        axis.title.y = element_blank(),
+                                                                                                                        axis.ticks.y = element_blank(),
+                                                                                                                        axis.text.y = element_blank(),
+                                                                                                                        axis.line.y = element_blank(),
+                                                                                                                        plot.margin = margin(2, 0, 2, 0)
+                                                                                                                      ) + scale_x_continuous(expand = c(0.001, 0.001))
+  
+  yax <- rep("A", length(gene))
+  inid <- which(gl %in% gene)
+  oid <- which(!gl %in% gene)
+  yaxglid <-
+    round(seq(length(gene)*0.1, length(gene)*0.9, length.out = length(inid)))
+  yax[yaxglid] <- gl
+  yax[setdiff(1:length(yax), yaxglid)] <- setdiff(gene, gl)
+  gl <- gl[order(match(gl,levels(plotdata$Gene)))]
+  p2 <-
+    ggplot() + geom_point(data = data.frame(gene = factor(yax, levels = yax), x =
+                                              1),
+                          aes(x = x, y = gene),
+                          col = "white") + geom_text(data = data.frame(text = gl, id = gl, x = 1),
+                                                     aes(x = x, y = id, label = gl),
+                                                     size = 3) + geom_segment(
+                                                       data = data.frame(
+                                                         x = 1.5,
+                                                         xend = 2,
+                                                         y = gl,
+                                                         yend = yax[match(gl, geneorder)]
+                                                       ),
+                                                       aes(
+                                                         x = x,
+                                                         y = y,
+                                                         xend = xend,
+                                                         yend = yend
+                                                       ),
+                                                       size = 0.1
+                                                     ) + theme_classic() + coord_cartesian(xlim = c(0.5, 2)) + theme(
+                                                       axis.title = element_text(color = "white"),
+                                                       axis.line = element_line(color = "white"),
+                                                       axis.text = element_text(color = "white"),
+                                                       axis.ticks = element_line(color = "white"),
+                                                       plot.margin = margin(2, 0, 2, 2)
+                                                     ) + scale_x_continuous(expand = c(0, 0))
+  gridExtra::grid.arrange(p2, p1, nrow = 1, layout_matrix = cbind(1, 2, 2, 2))
+}
+
