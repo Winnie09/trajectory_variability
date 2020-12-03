ec7d228074de7fc87a40c6556b977b5bcfeb0923
Wenpin Hou <wp.hou3@gmail.com>
2020-12-03 08:52:27 -0500

    add change

--- function/mySTIP2.R
+++ function/mySTIP2.R
@@ -1,4 +1,4 @@
-mySTIP2 <- function(fit, gl) {
+mySTIP2 <- function(fit, gl, plot = TRUE, ReturnGeneOrder = FALSE) {
   ## this function will show all gene lables of gl. Heatmap is similar to STIP, but only contains gl genes.
   ## fit: gene by cell matrix. colnames: pseudotime of the cells. rownames: gene names.
   dn <- dimnames(fit)
@@ -45,19 +45,24 @@ mySTIP2 <- function(fit, gl) {
     c(geneorder, names(sort(sapply(zp[m1], function(i)
       match(i[1],colnames(fit))))))
   geneorder <- rev(geneorder)
-  plotdata <- fit[geneorder,]
-  plotdata <- melt(plotdata)
-  colnames(plotdata) <- c("Gene", "Pseudotime", "Expression")
-  plotdata$Pseudotime <- match(plotdata$Pseudotime,colnames(fit))
-  gl <- gl[order(match(gl, levels(plotdata$Gene)))]
-  p <-
-    ggplot(plotdata, aes(Pseudotime, Gene, fill = Expression)) + geom_tile() + theme_classic() + scale_fill_gradient2(low ="blue",high = "red",midpoint = 0) + theme(
-                                                                                                                        axis.title.y = element_blank(),
-                                                                                                                        axis.ticks.y = element_blank(),
-                                                                                                                        axis.line.y = element_blank(),
-                                                                                                                        plot.margin = margin(2, 0, 2, 0)
-                                                                                                                      ) + scale_x_continuous(expand = c(0.001, 0.001))
-  return(p)
+  if (ReturnGeneOrder) {
+    return(geneorder)
+  } else {
+    plotdata <- fit[geneorder,]
+    plotdata <- melt(plotdata)
+    colnames(plotdata) <- c("Gene", "Pseudotime", "Expression")
+    plotdata$Pseudotime <- match(plotdata$Pseudotime,colnames(fit))
+    gl <- gl[order(match(gl, levels(plotdata$Gene)))]
+    p <-
+      ggplot(plotdata, aes(Pseudotime, Gene, fill = Expression)) + geom_tile() + theme_classic() + scale_fill_gradient2(low ="blue",high = "red",midpoint = 0) + theme(
+                                                                                                                          axis.title.y = element_blank(),
+                                                                                                                          axis.ticks.y = element_blank(),
+                                                                                                                          axis.line.y = element_blank(),
+                                                                                                                          plot.margin = margin(2, 0, 2, 0)
+                                                                                                                        ) + scale_x_continuous(expand = c(0.001, 0.001))
+    return(p)
+  }
 }
+
 
 
