9894f253ff64fc7ffc0581a46efc772d998d518a
Wenpin Hou <wp.hou3@gmail.com>
2020-12-04 14:52:25 -0500

    testvar get perf

--- hca/simu/testtime/old_data_poolSampleSignal/code/02_addsignal_test.R
+++ hca/simu/testtime/old_data_poolSampleSignal/code/02_addsignal_test.R
@@ -1,28 +1,30 @@
 # --------------
 # global setting
 # --------------
-dataType <- as.numeric(commandArgs(trailingOnly = T)[[1]])
-method <- as.character(commandArgs(trailingOnly = T)[[2]])
-# dataType <- 1
-# method <- 'EM_NOT_centered'
-print(paste0('dataType', dataType))
-print(method)
+clusterType <- as.numeric(commandArgs(trailingOnly = T)[[1]])
+pctGene <- as.numeric(commandArgs(trailingOnly = T)[[2]])
+method <- as.character(commandArgs(trailingOnly = T)[[3]])
+# clusterType <- 1
+# pctGene <- 1
+# method <- 'tradeSeq'
 
+print(method)
+print(paste0('clusterType', clusterType, '_', pctGene))
+
+setwd('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/')
+ddir <- './hca/data/simu/testtime/poolSampleSignal/'
+rdir <- './hca/simu/testtime/result/addsignal/'
+dir.create(paste0(rdir,method), recursive = T, showWarnings = F)
+source('./function/01_function.R')
+source('/home-4/whou10@jhu.edu/scratch/Wenpin/resource/myfunc/01_function.R')
 suppressMessages(library(parallel))
 suppressMessages(library(splines))
 suppressMessages(library(limma))
-library(here)
-here()
-source('/home-4/whou10@jhu.edu/scratch/Wenpin/resource/myfunc/01_function.R')
-source(here('function/01_function.R'))
-ddir <- here('hca/data/simu/testtime/addMultiSignalUsingExpr/')
-rdir <- here('hca/simu/testtime/result/addsignal/')
-dir.create(paste0(rdir,method), recursive = T, showWarnings = F)
 
 # ------------
 # prepare data
 # ------------
-pt <- readRDS(here('hca/data/simu/testtime/poolSampleSignal/null/pseudotime.rds'))
+pt <- readRDS('./hca/data/simu/testtime/poolSampleSignal/null/pseudotime.rds')
 pseudotime <- pt[,2]
 names(pseudotime) <- pt[,1]
 
@@ -33,7 +35,9 @@ if (grepl('tradeSeq', method)){
   suppressMessages(library(SingleCellExperiment))
   suppressMessages(library(slingshot))
   suppressMessages(library(tradeSeq))
-  expr <- readRDS(paste0(ddir, 'count/', dataType, '.rds'))
+  expr <- readRDS(paste0(ddir, 'count/clusterType', clusterType, '_', pctGene, '.rds'))
+  design = matrix(rep(1,8), nrow=8)
+  dimnames(design) = list(paste0('BM',seq(1,8)), 'intercept')
   cellanno = data.frame(cell=colnames(expr), sample = sub(':.*','', colnames(expr)), stringsAsFactors = FALSE)
   
   ##
@@ -51,8 +55,8 @@ if (grepl('tradeSeq', method)){
               nknots = 6, verbose = FALSE,parallel=F)
   # sce <- fitGAM(counts = round(expr), pseudotime = pdt, cellWeights = cellWeights,
   #               nknots = 6, verbose = FALSE,parallel=TRUE, BPPARAM = MulticoreParam(2))
-  saveRDS(sce, paste0(rdir, method,'/', dataType,'_sce.rds'))
-  res <- list()
+  saveRDS(sce, paste0(rdir, method,'/clusterType', clusterType, '_', pctGene,'_sce.rds'))
+  Final <- list()
   for (TestType in (c('startVsEndTest', 'associationTest'))){
     print(TestType)
     if (grepl('startVsEndTest', TestType)){
@@ -60,58 +64,61 @@ if (grepl('tradeSeq', method)){
     } else if (grepl('associationTest', TestType)){
       Res <- associationTest(sce)
     } 
-    resdf <- data.frame(waldStat = Res[,'waldStat'], P.Value = Res[,'pvalue'] ,adj.P.Val = p.adjust(Res$pvalue, method='fdr'))
-    row.names(resdf) <- row.names(Res)
-    resdf <- resdf[order(resdf[,3], -resdf[,1]), ]
-    res[[TestType]] <- resdf
+    res <- data.frame(waldStat = Res[,'waldStat'], P.Value = Res[,'pvalue'] ,adj.P.Val = p.adjust(Res$pvalue, method='fdr'))
+    row.names(res) <- row.names(Res)
+    res <- res[order(res[,3], -res[,1]), ]
+    Final[[TestType]] <- res
   }
-  
+  saveRDS(Final, paste0(rdir, method,'/clusterType', clusterType, '_', pctGene,'.rds'))  
 }
 
 if (method == 'EM_centered'){
-  expr <- readRDS(paste0(ddir, 'saver/', dataType, '.rds'))
+  expr <- readRDS(paste0(ddir, 'saver/clusterType', clusterType, '_', pctGene, '.rds'))
   expr <- log2(expr + 1)
   expr <- expr[rowMeans(expr > 0.1) > 0.1, ]
   design = matrix(rep(1,8), nrow=8)
   dimnames(design) = list(paste0('BM',seq(1,8)), c('intercept'))
   cellanno = data.frame(cell=colnames(expr), sample = sub(':.*','', colnames(expr)), stringsAsFactors = FALSE)
-  res <- testpt(expr=expr,cellanno=cellanno,pseudotime=pseudotime,design=design,ncores=1, permuiter=100, type = 'Time')
+  # design = cbind(1,design)
+  testres <- testpt(expr=expr,cellanno=cellanno,pseudotime=pseudotime,design=design,ncores=8, permuiter=100, type = 'Time')
+  saveRDS(testres, paste0(rdir, method,'/clusterType', clusterType, '_', pctGene,'.rds'))  
 }
 
 
 if (method == 'EM_NOT_centered'){
-  expr <- readRDS(paste0(ddir, 'saver/', dataType, '.rds'))
+  expr <- readRDS(paste0(ddir, 'saver/clusterType', clusterType, '_', pctGene, '.rds'))
   expr <- log2(expr + 1)
   expr <- expr[rowMeans(expr > 0.1) > 0.1, ]
   design = matrix(rep(1,8), nrow=8)
   dimnames(design) = list(paste0('BM',seq(1,8)), c('intercept'))
   cellanno = data.frame(cell=colnames(expr), sample = sub(':.*','', colnames(expr)), stringsAsFactors = FALSE)
-  res <- testpt(expr=expr,cellanno=cellanno,pseudotime=pseudotime,design=design,ncores=7, permuiter=100, type = 'Time', demean = FALSE, return.all.data = FALSE)
+  # design = cbind(1,design)
+  testres <- testpt(expr=expr,cellanno=cellanno,pseudotime=pseudotime,design=design,ncores=8, permuiter=100, type = 'Time', demean = FALSE)
+  saveRDS(testres, paste0(rdir, method,'/clusterType', clusterType, '_', pctGene,'.rds'))  
 }
 
 
 if (method == 'tscan'){
-  expr <- readRDS(paste0(ddir, 'saver/', dataType, '.rds'))
+  expr <- readRDS(paste0(ddir, 'saver/clusterType', clusterType, '_', pctGene, '.rds'))
   expr <- expr[, names(pseudotime)]
   res <- TSCAN_time(expr=expr,pseudotime=pseudotime)
+  saveRDS(res, paste0(rdir, method,'/clusterType', clusterType, '_', pctGene,'.rds'))  
 }
 
 if (method == 'monocle2'){
-  expr <- readRDS(paste0(ddir, 'saver/', dataType, '.rds'))
+  expr <- readRDS(paste0(ddir, 'saver/clusterType', clusterType, '_', pctGene, '.rds'))
   expr <- expr[, names(pseudotime)]
   res <- monocle2_time(expr=expr,pseudotime=pseudotime)
+  saveRDS(res, paste0(rdir, method,'/clusterType', clusterType, '_', pctGene,'.rds'))  
 }
 
 if (method == 'monocle3'){
   library(spdep)
-  expr <- readRDS(paste0(ddir, 'saver/', dataType, '.rds'))
+  expr <- readRDS(paste0(ddir, 'saver/clusterType', clusterType, '_', pctGene, '.rds'))
   expr <- expr[, names(pseudotime)]
   pca = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/data/simu/testtime/poolSampleSignal/null/hsc_mep_ery_integrated_pca.rds')
   res <- monocle3_time(expr=expr, cell_coords = pca[,1:4])
+  saveRDS(res, paste0(rdir, method,'/clusterType', clusterType, '_', pctGene,'.rds'))  
 }
-
-saveRDS(res, paste0(rdir, method,'/', dataType,'.rds'))  
-
-
 
 
--- hca/simu/testvar/addMultiSignalUsingExpr/code/03_perf.R
+++ hca/simu/testvar/addMultiSignalUsingExpr/code/03_perf.R
@@ -5,7 +5,7 @@ rdir <- paste0(here('hca','simu', 'testvar', 'addMultiSignalUsingExpr', 'perf'),
 selgene <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/data/simu/testvar/addMultiSignalUsingExpr/selgene/selgene.rds')
 source('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/function/01_function.R')
 
-m = 'EM'
+m = 'EM_centered'
 af = list.files(paste0(ddir, m, '/'))
 df1 <- sapply(af, function(f){
   print(f)
@@ -16,6 +16,16 @@ df1 <- sapply(af, function(f){
 })
 df1 = t(df1)
 
+m = 'EM_NOT_centered'
+af = list.files(paste0(ddir, m, '/'))
+df1.nc <- sapply(af, function(f){
+  print(f)
+  r = readRDS(paste0(ddir, m, '/', f))
+  res = data.frame(adj.P.Val = r[['fdr']], foldchange = r[['foldchange']], stringsAsFactors = F)
+  res = res[order(res[,1], -res[,2]), ]
+  c(sub('.rds','',f), 'EM_NOT_centered', AreaUnderSensFdr(SensFdr(selgene, res)))
+})
+df1.nc = t(df1.nc)
 
 m = 'EM_meandiff'
 af = list.files(paste0(ddir, m, '/'))
@@ -27,47 +37,85 @@ df2 <- sapply(af, function(f){
 })
 df2 = t(df2)
 
-
-## merge trend diff and mean diff, get the minimum fdr as the fdr for each gene
-af = list.files(paste0(ddir, 'EM/'))
+## merge trend diff (centered) and mean diff, get the minimum fdr as the fdr for each gene
+af = list.files(paste0(ddir, 'EM_centered/'))
 df.merge <- sapply(af, function(f){
   print(f)
-  r = readRDS(paste0(ddir, 'EM/', f))
+  r = readRDS(paste0(ddir, 'EM_centered/', f))
   res1 = data.frame(adj.P.Val = r[['fdr']], foldchange = r[['foldchange']], stringsAsFactors = F)
   r = readRDS(paste0(ddir, 'EM_meandiff/', f))
   res2 = r[,c('adj.P.Val', 'logFC')]
   colnames(res2) <- colnames(res1)
-  
-  res <- lapply(1:nrow(res1), function(i){
-    if (res1[i,1] == res2[i,1] & res1[i,2] > res2[i,2]) {
-      res1[i,]
-    } else if (res1[i,1] < res2[i,1]){
-      res1[i,]
-    }
-    else {
-      res2[i,]
+  res2 <- res2[rownames(res1), ]
+  res <- lapply(rownames(res1), function(i){
+    if (res1[i,1] <= res2[i,1]) {
+      unlist(res1[i,,drop=T])
+    } else {
+      c(res2[i,1], res1[i,2])
     }
   })
   res <- do.call(rbind, res)
+  rownames(res) <- rownames(res1)
   res = res[order(res[,1], -res[,2]), ]
   c(sub('.rds','',f), 'EM_merge_min', AreaUnderSensFdr(SensFdr(selgene, res)))
 })
 df.merge.min = t(df.merge)
 
-## merge trend diff and mean diff, 1-(1-fdr1)(1-fdr2): under null,  the probability of oberving the data or more extreme cases in at least one situation
-af = list.files(paste0(ddir, 'EM/'))
+## merge trend diff (centered) and mean diff, 1-(1-fdr1)(1-fdr2): under null,  the probability of oberving the data or more extreme cases in at least one situation
+af = list.files(paste0(ddir, 'EM_centered/'))
 df.merge <- sapply(af, function(f){
   print(f)
-  r1 = readRDS(paste0(ddir, 'EM/', f))
+  r1 = readRDS(paste0(ddir, 'EM_centered/', f))
   
   r2 = readRDS(paste0(ddir, 'EM_meandiff/', f))
-  res <- data.frame(fdr = 1 - (1 - r1[['fdr']]) * (1 - r2[,'adj.P.Val']), stringsAsFactors = FALSE)
+  r2 <- r2[names(r1[['fdr']]),]
+  res <- data.frame(fdr = 1 - (1 - r1[['fdr']]) * (1 - r2[,'adj.P.Val']), 
+                    foldchange = r1[['foldchange']],
+                    stringsAsFactors = FALSE)
   rownames(res) <- names(r1[['fdr']])
-  res = res[order(res[,1]), ,drop = FALSE]
+  res = res[order(res[,1], res[,2]), ,drop = FALSE]
   c(sub('.rds','',f), 'EM_merge_atleast', AreaUnderSensFdr(SensFdr(selgene, res)))
 })
 df.merge.atleast = t(df.merge)
 
+
+## merge trend diff (centered) and mean diff, 1- fdr1 * fdr2: under null,  the probability of oberving the data or more extreme cases in at least one situation
+af = list.files(paste0(ddir, 'EM_centered/'))
+df.merge2 <- sapply(af, function(f){
+  print(f)
+  r1 = readRDS(paste0(ddir, 'EM_centered/', f))
+  
+  r2 = readRDS(paste0(ddir, 'EM_meandiff/', f))
+  identical(names(r1[['fdr']]), rownames(r2[names(r1[['fdr']]),]))
+  
+  res <- data.frame(fdr = r1[['fdr']] * r2[names(r1[['fdr']]),'adj.P.Val'] -1, 
+                    foldchange = r1[['foldchange']],
+                    stringsAsFactors = FALSE)
+  rownames(res) <- names(r1[['fdr']])
+  res = res[order(res[,1], -res[,2]), ,drop = FALSE]
+  c(sub('.rds','',f), 'EM_merge_atleast_1_fdr1fdr2', AreaUnderSensFdr(SensFdr(selgene, res)))
+})
+df.merge.atleast_1_fdr1fdr2 = t(df.merge2)
+
+
+## merge NOT-centered and mean diff, 1- fdr1 * fdr2: under null,  the probability of oberving the data or more extreme cases in at least one situation
+af = list.files(paste0(ddir, 'EM_NOT_centered/'))
+df.merge <- sapply(af, function(f){
+  print(f)
+  r1 = readRDS(paste0(ddir, 'EM_NOT_centered/', f))
+  
+  r2 = readRDS(paste0(ddir, 'EM_meandiff/', f))
+  identical(names(r1[['fdr']]), rownames(r2[names(r1[['fdr']]),]))
+  
+  res <- data.frame(fdr = r1[['fdr']] * r2[names(r1[['fdr']]),'adj.P.Val'] -1, 
+                    foldchange = r1[['foldchange']],
+                    stringsAsFactors = FALSE)
+  rownames(res) <- names(r1[['fdr']])
+  res = res[order(res[,1], -res[,2]), ,drop = FALSE]
+  # plot(res[,1], res[,2], xlab='1-fdr1*fdr2',ylab='trenddiff fc (orill-perll)', pch=20)
+  c(sub('.rds','',f), 'EM_NOT_centered_1_fdr1fdr2', AreaUnderSensFdr(SensFdr(selgene, res)))
+})
+df.nc.1_fdr1fdr2 = t(df.merge)
 # ------------------
 # other methods 
 # ------------------
@@ -122,15 +170,13 @@ df6 <- sapply(af, function(f){
 })
 df6 = t(df6)
 
-res <- rbind(df1, df2, df3, df4, df5, df6)
-colnames(res) <- c('Signal.Strength', 'Method', 'Fdr.Diff', 'AUC')
+## gather all results
+res <- rbind(df1, df1.nc, df2, df3, df4, df5, df6)
+colnames(res) <- c('SignalStrength', 'Method', 'Fdr.Diff', 'AUC')
 
 ## add EM merge results
-a = rbind(df.merge.min, df.merge.atleast)
+a = rbind(df.merge.min, df.merge.atleast, df.merge.atleast_1_fdr1fdr2, df.nc.1_fdr1fdr2)
 b = data.frame(a[,1], a[,2], a[,3], a[,4], stringsAsFactors = F)
 colnames(b) <- colnames(res)
-res2 = rbind(res,b)
-saveRDS(res2, paste0(rdir, 'perf.rds'))
-
-
-
+Res = rbind(res,b)
+saveRDS(Res, paste0(rdir, 'perf.rds'))
