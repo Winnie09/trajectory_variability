6a70c474e87699189d58694d0088d3159e24fff9
Wenpin Hou <wp.hou3@gmail.com>
2020-12-11 09:35:02 -0500

    add testtime fit plot

--- function/plotGenePopulation.R
+++ function/plotGenePopulation.R
@@ -1,14 +1,33 @@
 plotGenePopulation <- function(testobj,
                                gene,
-                               variable = NULL){
-    ## testobj: object returned from testpt(). testvar is ready. testtime is not ready (should adjust the fit data structure a bit).
+                               variable = NA){
+    ## testobj: object returned from testpt(). 
     ## gene: a character vector of gene names. It can be of length 1 or > 1.
-    ## variable: a character (within the column names in design matrix) to get population pattern.
-    if (is.null(variable)){
-    print('variable = NULL, but testtime is not ready.')
+    ## variable: a character (within the column names in design matrix) to get population pattern. If variable == NA, then return testtime fit. Other wise return testvar fit with the variable.
+    if (is.na(variable)){
+      fit <- sapply(gene, function(g){
+        tmp <- get_population_fit(testobj, variable = NA, gene = g)
+      }, simplify = FALSE)
+      pd <- sapply(1:length(fit), function(i){
+            tmp <- reshape2::melt(fit[[i]])
+            colnames(tmp) <- c('expression')
+            tmp <- data.frame(tmp, 
+                             gene = names(fit)[i], 
+                             pseudotime = 1:length(fit[[i]]), 
+                             stringsAsFactors = FALSE)
+      }, simplify = FALSE)
+      pd <- do.call(rbind, pd)
+      pd$gene <- as.factor(pd$gene)
+      p <- ggplot2::ggplot(data= pd, aes(x = pseudotime, y = expression, color = 'red')) + 
+              geom_point() +
+              theme_classic() +
+              facet_wrap(~gene, nrow = round(sqrt(length(gene))))+
+              xlab('Pseudotime') +
+              ylab('Expression') +
+              labs(color = '')
   } else {
       fit <- sapply(gene, function(g){
-        tmp <- get_population_fit(Res, variable = variable, gene = g)
+        tmp <- get_population_fit(testobj, variable = variable, gene = g)
       }, simplify = FALSE)
       
       pd <- sapply(1:length(fit), function(i){
