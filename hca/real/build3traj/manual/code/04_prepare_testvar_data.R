154a3aa8bda6dffc8060ee216cac5ac922c08a0b
Wenpin Hou <wp.hou3@gmail.com>
2020-12-27 10:41:18 -0500

    prepare testvar data

---
+++ hca/real/build3traj/manual/code/04_prepare_testvar_data.R
@@ -0,0 +1,23 @@
+library(here)
+setwd(here())
+expr <- readRDS('./hca/data/proc/matrix/saver.rds') ## [1:22401, 1:32819] 
+for (path in c('lymph', 'monocyte', 'erythroid')){
+  rdir <- paste0('./hca/real/build3traj/manual/result/', path, '/')
+  print(rdir)
+  pt <- readRDS(paste0(rdir, 'pseudotime.rds'))
+  m <- expr[, pt]
+  m <- m[rowMeans(m > 0.1) > 0.01, ]
+  cellanno = data.frame(cell = colnames(m), sample = sub('_.*','', colnames(m)), stringsAsFactors = FALSE)
+  design = data.frame(intercept = 1,          
+                      gender = sub('.*:','', unique(cellanno$sample)),
+                      age = sapply(unique(cellanno$sample), function(i) strsplit(i, ':')[[1]][2]),
+                      stringsAsFactors = FALSE)
+  rownames(design) <- unique(cellanno$sample)
+  print(design)
+  pseudotime = seq(1, length(pt))
+  names(pseudotime) <- pt
+  saveRDS(m, paste0(rdir,'input_expr.res'))
+  saveRDS(cellanno, paste0(rdir,'input_cellanno.rds'))
+  saveRDS(design, paste0(rdir,'input_design.rds'))
+  saveRDS(pseudotime, paste0(rdir,'input_pseudotime.rds'))
+}
