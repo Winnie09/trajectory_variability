ca4bd26479f85dd5cb9f786c67142463b2b9b9a6
Wenpin Hou <wp.hou3@gmail.com>
2021-01-01 20:21:30 -0500

    add changes

--- covid/Su_2020_Cell/testvar/useNotImp/code/01_numeric_HD_others.R
+++ covid/Su_2020_Cell/testvar/useNotImp/code/01_numeric_HD_others.R
@@ -1,6 +1,6 @@
 library(here)
 setwd(here())
-expr <- readRDS('covid/Su_2020_Cell/data/log2norm.rds')
+expr <- readRDS('covid/Su_2020_Cell/data/log2norm_my.rds')
 pt <- readRDS('covid/Su_2020_Cell/data/tActivate_pseudotime.rds')
 meta <- readRDS('covid/Su_2020_Cell/data/meta.rds')
 cellanno <- readRDS('covid/Su_2020_Cell/data/cellanno.rds')
@@ -8,11 +8,12 @@ cellanno <- readRDS('covid/Su_2020_Cell/data/cellanno.rds')
 # design <- readRDS('covid/Su_2020_Cell/data/design_numeric_HD_Se.rds')
 design <- readRDS('covid/Su_2020_Cell/data/design_numeric_HD_others.rds')
 expr <- expr[rowMeans(expr>0.1)>0.01, ]
+expr <- expr[, cellanno[,1]]
 
 source('function/01_function.R')
 rdir <- 'covid/Su_2020_Cell/testvar/result/'
 system.time({
-  res <- testpt(expr=expr, cellanno=cellanno, pseudotime=pt, design=design, type='Variable', ncores = 4, demean = FALSE)
+  res <- testpt.seed(expr=expr, cellanno=cellanno, pseudotime=pt, design=design, type='Variable', ncores = 4, demean = FALSE)
 })
 saveRDS(res, paste0(rdir, 'numeric_HD_others_res.rds'))
 
