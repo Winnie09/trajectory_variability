cf52f2a3322a677541a67de5d0c2a28a51357c98
Wenpin Hou <wp.hou3@gmail.com>
2021-01-04 17:06:07 -0500

    change yaxis

--- hca/simu/testvar/addMultiSignalUsingExpr/code/04_plot_perf.R
+++ hca/simu/testvar/addMultiSignalUsingExpr/code/04_plot_perf.R
@@ -1,24 +1,126 @@
-pdir <- '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/simu/testvar/addMultiSignalUsingExpr/plot/perf/'
-pd <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/simu/testvar/addMultiSignalUsingExpr/result/perf/perf.rds')
-pd <- as.data.frame(pd)
-#perf <- perf[perf[,2]!='EM_centered',]
-pd$SignalStreghth <- as.numeric(pd$Type)
+rm(list=ls())
+library(here)
+setwd(here())
+# setwd('/Users/wenpinhou/Dropbox/trajectory_variability')
+pdir <- 'hca/simu/testvar/addMultiSignalUsingExpr/plot/perf/'
+dir.create(pdir)
+## overall performance
+pd <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/result/perf/perf.rds')
+str(pd)
+pd$Method <- as.character(pd$Method)
+pd <- pd[pd$Method != 'trenddiff', ]
+
+pd$Method[as.character(pd$Method) == 'EM_NOT_centered'] <- 'TrajDiffTest'
+pd$Method[pd$Method == 'meandiff'] <- 'MeanDiffTest'
+pd[,1] <- as.numeric(pd[,1])
 pd[,3] = as.numeric(as.character(pd[,3]))
 pd[,4] = as.numeric(as.character(pd[,4]))
 library(ggplot2)
 library(gridExtra)
-pdf(paste0(pdir, 'compare_fdr_diff_auc.pdf'),width=10,height=3.2)
-p1 <- ggplot(pd, aes(x = SignalStreghth, y = Fdr.Diff, color=Method)) + 
+p1 <- ggplot(pd, aes(x = SignalStrength, y = Fdr.Diff, color=Method)) + 
   geom_point(size=3)  + 
   geom_line(size=1) + 
   theme_classic() + 
-  ylab('fdr.diff(real~reported - 0.25*0.25/2)') +
+  # ylab('FDR.diff(real~reported - 0.25*0.25/2)') +
+  ylab('RealFDR - ReportedFDR') +
   scale_color_brewer(palette = 'Dark2')  
-p2 <- ggplot(pd, aes(x = SignalStreghth, y = AUC, color=Method)) + 
+p2 <- ggplot(pd, aes(x = SignalStrength, y = AUC, color=Method)) + 
   geom_point(size=3)  + 
   geom_line(size=1) + 
   theme_classic() + 
-  ylab('Area Under Sensitivity-RealFDR curve') +
+  # ylab('Area Under Sensitivity-RealFDR Curve') +
+  ylab('AUC') + 
   scale_color_brewer(palette = 'Dark2') 
+pdf(paste0(pdir, 'compare_fdr_diff_auc.pdf'),width=7,height=2.2)
 gridExtra::grid.arrange(p1,p2,nrow=1)
 dev.off()
+
+## mean only
+pd <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/result/perf/perf_meanOnly.rds')
+str(pd)
+pd$Method <- as.character(pd$Method)
+pd <- pd[pd$Method != 'trenddiff', ]
+pd$Method[as.character(pd$Method) == 'EM_NOT_centered'] <- 'TrajDiffTest'
+pd$Method[pd$Method == 'meandiff'] <- 'MeanDiffTest'
+
+pd[,1] <- as.numeric(pd[,1])
+pd[,3] = as.numeric(as.character(pd[,3]))
+pd[,4] = as.numeric(as.character(pd[,4]))
+library(ggplot2)
+library(gridExtra)
+p1 <- ggplot(pd, aes(x = SignalStrength, y = Fdr.Diff, color=Method)) + 
+  geom_point(size=3)  + 
+  geom_line(size=1) + 
+  theme_classic() + 
+  ylab('RealFDR - ReportedFDR') +
+  scale_color_brewer(palette = 'Dark2')  
+p2 <- ggplot(pd, aes(x = SignalStrength, y = AUC, color=Method)) + 
+  geom_point(size=3)  + 
+  geom_line(size=1) + 
+  theme_classic() + 
+   ylab('AUC') + 
+  scale_color_brewer(palette = 'Dark2') 
+pdf(paste0(pdir, 'compare_fdr_diff_auc_meanOnly.pdf'),width=7,height=2.2)
+gridExtra::grid.arrange(p1,p2,nrow=1)
+dev.off()
+
+## trend Only
+pd <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/result/perf/perf_trendOnly.rds')
+str(pd)
+pd$Method <- as.character(pd$Method)
+pd <- pd[pd$Method != 'trenddiff', ]
+pd$Method[as.character(pd$Method) == 'EM_NOT_centered'] <- 'TrajDiffTest'
+pd$Method[pd$Method == 'meandiff'] <- 'MeanDiffTest'
+pd[,1] <- as.numeric(pd[,1])
+pd[,3] = as.numeric(as.character(pd[,3]))
+pd[,4] = as.numeric(as.character(pd[,4]))
+library(ggplot2)
+library(gridExtra)
+p1 <- ggplot(pd, aes(x = SignalStrength, y = Fdr.Diff, color=Method)) + 
+  geom_point(size=3)  + 
+  geom_line(size=1) + 
+  theme_classic() + 
+  ylab('RealFDR - ReportedFDR') +
+  scale_color_brewer(palette = 'Dark2')  
+p2 <- ggplot(pd, aes(x = SignalStrength, y = AUC, color=Method)) + 
+  geom_point(size=3)  + 
+  geom_line(size=1) + 
+  theme_classic() + 
+  # ylab('Area Under Sensitivity-RealFDR Curve') +
+   ylab('AUC') + 
+  scale_color_brewer(palette = 'Dark2') 
+pdf(paste0(pdir, 'compare_fdr_diff_auc_trendOnly.pdf'),width=7,height=2.2)
+gridExtra::grid.arrange(p1,p2,nrow=1)
+dev.off()
+
+## trend Mean
+pd <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/result/perf/perf_trendMean.rds')
+str(pd)
+pd$Method <- as.character(pd$Method)
+pd <- pd[pd$Method != 'trenddiff', ]
+pd$Method[as.character(pd$Method) == 'EM_NOT_centered'] <- 'TrajDiffTest'
+pd$Method[pd$Method == 'meandiff'] <- 'MeanDiffTest'
+pd[,1] <- as.numeric(pd[,1])
+pd[,3] = as.numeric(as.character(pd[,3]))
+pd[,4] = as.numeric(as.character(pd[,4]))
+library(ggplot2)
+library(gridExtra)
+p1 <- ggplot(pd, aes(x = SignalStrength, y = Fdr.Diff, color=Method)) + 
+  geom_point(size=3)  + 
+  geom_line(size=1) + 
+  theme_classic() + 
+  # ylab('FDR.diff(real~reported - 0.25*0.25/2)') +
+  ylab('RealFDR - ReportedFDR') +
+  scale_color_brewer(palette = 'Dark2')  
+p2 <- ggplot(pd, aes(x = SignalStrength, y = AUC, color=Method)) + 
+  geom_point(size=3)  + 
+  geom_line(size=1) + 
+  theme_classic() + 
+  ylab('AUC') +
+  scale_color_brewer(palette = 'Dark2') 
+pdf(paste0(pdir, 'compare_fdr_diff_auc_trendMean.pdf'),width=7,height=2.2)
+gridExtra::grid.arrange(p1,p2,nrow=1)
+dev.off()
+
+
+
