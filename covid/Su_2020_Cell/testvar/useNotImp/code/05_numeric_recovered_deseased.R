717fbb8c328ab399c09b3d6f3f11c348e89f5ef0
Wenpin Hou <wp.hou3@gmail.com>
2021-01-12 11:39:30 -0500

    compare recovered vs. deceased

---
+++ covid/Su_2020_Cell/testvar/useNotImp/code/05_numeric_recovered_deseased.R
@@ -0,0 +1,24 @@
+library(here)
+setwd(here())
+expr <- readRDS('covid/Su_2020_Cell/data/log2norm_my.rds')
+pt <- readRDS('covid/Su_2020_Cell/data/tActivate_pseudotime.rds')
+meta <- readRDS('covid/Su_2020_Cell/data/meta.rds')
+cellanno <- readRDS('covid/Su_2020_Cell/data/cellanno.rds')
+design <- readRDS('covid/Su_2020_Cell/data/design_numeric_Recovered_Deceased.rds')
+
+
+rownames(cellanno) <- cellanno[,1]
+cellanno <- cellanno[pt, ]
+cellanno <- cellanno[cellanno[,2] %in% rownames(design), ]
+pt <- pt[names(pt) %in% cellanno[,1]]
+expr <- expr[, names(pt)]
+expr <- expr[rowMeans(expr>0.1)>0.01, ]
+expr <- expr[, cellanno[,1]]
+
+source('function/01_function.R')
+rdir <- 'covid/Su_2020_Cell/testvar/result/'
+system.time({
+  res <- testpt.seed(expr=expr, cellanno=cellanno, pseudotime=pt, design=design, type='Variable', ncores = 4, demean = FALSE)
+})
+saveRDS(res, paste0(rdir, 'numeric_Recovered_Deceased_res.rds'))
+
