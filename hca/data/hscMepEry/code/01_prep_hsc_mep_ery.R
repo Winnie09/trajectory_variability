ac09c56dba8277439fa2ae6eb158bc230538ea05
Wenpin Hou <wp.hou3@gmail.com>
2021-01-16 14:49:09 -0500

    01_lymph.R

--- hca/data/hscMepEry/code/01_prep_hsc_mep_ery.R
+++
@@ -1,8 +0,0 @@
-mat = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/data/HCA/proc/matrix/normcount.rds')
-ct = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/data/HCA/proc/ct/sc.rds')
-ct = ct[colnames(mat)]
-smat = mat[,ct == 'HSC' | ct == 'MEP' | ct == 'Ery']
-saveRDS(smat,'/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/simu/data/hscMepEry/matrix/normcount.rds')
-ct  = ct[colnames(smat)]
-saveRDS(ct,'/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/simu/data/hscMepEry/ct/ct.rds')
-
--- hca/data/hscMepEry/code/02_pca.R
+++
@@ -1,8 +0,0 @@
-smat = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/simu/data/hscMepEry/matrix/normcount.rds')
-# ct = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/simu/data/hscMepEry/ct/ct.rds')
-set.seed(12345)
-source('/home-4/whou10@jhu.edu/scratch/Wenpin/resource/function.R')
-setwd('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/simu/data/hscMepEry/pca/')
-set.seed(12345)
-pca = PCA(genebycell_mat=smat, save.pca = TRUE, plot.statistics=TRUE, plot.dir = getwd(), result.dir = getwd(), PC_for_columns = TRUE, findVariableGenes = TRUE, numPC = NULL)
-
