a05074ef73695e2edbec1f26b425badabe13e949
Wenpin Hou <wp.hou3@gmail.com>
2021-01-21 15:51:04 -0500

    plotClusterMean.R

--- function/plotClusterMean.R
+++ function/plotClusterMean.R
@@ -1,6 +1,9 @@
 plotClusterMean <- function(testobj, 
-                            cluster){
-  ## only works for Covariate Test. 
+                            cluster,
+                            type = 'time',
+                            facet = FALSE){
+  ## type: "time" (default) or "variable.
+  ## facet: plot each cluster indiviidually. Lnly works when type == 'time'. When type == 'variable', the plot will be facet anyway for each cluster.
   if ('populationFit' %in% names(testobj)){
     fit <- testobj$populationFit
   } else {
@@ -9,22 +12,42 @@ plotClusterMean <- function(testobj,
   library(ggplot2)
   library(RColorBrewer)
   library(reshape2)
-  int <- intersect(rownames(fit[[1]]), names(cluster))
-  clu <- cluster[int]
-  pd <- lapply(1:length(fit), function(i){
-    mat <- fit[[i]][int, ]
-    tmp <- sapply(sort(unique(clu)), function(i){
-      colMeans(mat[clu == i, , drop = FALSE])
+  if (type == 'variable'){
+    int <- intersect(rownames(fit[[1]]), names(cluster))
+    clu <- cluster[int]
+    pd <- lapply(1:length(fit), function(i){
+      mat <- fit[[i]][int, ]
+      tmp <- sapply(sort(unique(clu)), function(i){
+        colMeans(mat[clu == i, , drop = FALSE])
+      })
+      tmp <- melt(tmp)
+      colnames(tmp) <- c('pseudotime', 'cluster', 'populationFitClusterMean')
+      tmp <- data.frame(tmp, type = names(fit[i]))
     })
-    tmp <- melt(tmp)
-    colnames(tmp) <- c('pseudotime', 'cluster', 'populationFitClusterMean')
-    tmp <- data.frame(tmp, type = names(fit[i]))
-  })
-  pd <- do.call(rbind, pd)
-  pd$cluster <- factor(pd$cluster)
-  p <- ggplot(data = pd) + geom_line(aes(x = pseudotime, y = populationFitClusterMean, color = type), size = 1)+
-    theme_classic() + 
-    facet_wrap(~cluster)
+    pd <- do.call(rbind, pd)
+    pd$cluster <- factor(pd$cluster)
+    p <- ggplot(data = pd) + geom_line(aes(x = pseudotime, y = populationFitClusterMean, color = type), size = 1)+
+      theme_classic() + 
+      facet_wrap(~cluster)
+  } else if (type == 'time'){
+    int <- intersect(rownames(fit), names(cluster))
+    clu <- cluster[int]
+    
+      mat <- fit[int, ]
+      tmp <- sapply(sort(unique(clu)), function(i){
+        colMeans(mat[clu == i, , drop = FALSE])
+      })
+      pd <- melt(tmp)
+      colnames(pd) <- c('pseudotime', 'cluster', 'populationFitClusterMean')
+      
+    
+    pd$cluster <- factor(pd$cluster)
+    p <- ggplot(data = pd) + geom_line(aes(x = pseudotime, y = populationFitClusterMean, color = cluster), size = 1)+
+      theme_classic() 
+    if (facet){
+      p <- p + facet_wrap(~cluster)
+    }
+  }
   if (length(unique(pd$type)) < 8){
     p <- p + scale_color_brewer(palette = 'Dark2')
   } else {
@@ -32,5 +55,6 @@ plotClusterMean <- function(testobj,
   }
    print(p)
 }
+
 
 
