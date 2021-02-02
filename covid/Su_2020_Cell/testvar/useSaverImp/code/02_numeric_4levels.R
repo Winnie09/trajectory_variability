152e7d4c60d68e4ace214c557f140179dfd0a90f
Wenpin Hou <wp.hou3@gmail.com>
2021-02-02 07:42:52 -0500

    02_numeric_4levels.R

--- covid/Su_2020_Cell/testvar/useSaverImp/code/02_numeric_4levels.R
+++ covid/Su_2020_Cell/testvar/useSaverImp/code/02_numeric_4levels.R
@@ -11,9 +11,20 @@ expr <- expr[rowMeans(expr>0.1)>0.01, ]
 expr <- expr[, cellanno[,1]]
 source('function/01_function.R')
 rdir <- 'covid/Su_2020_Cell/testvar/useSaverImp/result/'
-system.time({
-  res <- testpt(expr=expr, cellanno=cellanno, pseudotime=pt, design=design, type='Variable', ncores = 10, demean = FALSE)
-})
-saveRDS(res, paste0(rdir, 'numeric_4levels_res.rds'))
+i = commandArgs(trailingOnly = T)[[1]][1]
+if (i == '1'){
+  system.time({
+    res <- testpt(expr=expr[1:4000, ], cellanno=cellanno, pseudotime=pt, design=design, type='Variable', ncores = 20, demean = FALSE)
+  })
+  saveRDS(res, paste0(rdir, 'numeric_4levels_res_1.rds'))
+} else {
+  system.time({
+    res <- testpt(expr=expr[4001:nrow(expr), ], cellanno=cellanno, pseudotime=pt, design=design, type='Variable', ncores = 20, demean = FALSE)
+  })
+  saveRDS(res, paste0(rdir, 'numeric_4levels_res_2.rds'))
+}
+
+
+  
 
 
