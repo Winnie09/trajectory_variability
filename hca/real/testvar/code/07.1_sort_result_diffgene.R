0e2e840e70bc8aa033abbf0b07b93aa9b54eee85
Wenpin Hou <wp.hou3@gmail.com>
2021-02-16 07:09:46 -0500

    07.1_sort_result_diffgene.R

--- hca/real/testvar/code/07.1_sort_result_diffgene.R
+++ hca/real/testvar/code/07.1_sort_result_diffgene.R
@@ -4,7 +4,12 @@ library(ggplot2)
 setwd('/Users/wenpinhou/Dropbox/trajectory_variability')
 # setwd(here())
 source('function/01_function.R')
-
+## read in gold standard 
+agediff <- readRDS('agediff/result/age_diffgene.rds')
+ChrX = readRDS('/Users/wenpinhou/Dropbox/resource/chrX_genename.rds')
+ChrY = readRDS('/Users/wenpinhou/Dropbox/resource/chrY_genename.rds')
+  
+  
 for (path in c('erythroid', 'lymph', 'monocyte')){
   ddir <- rdir <- paste0('hca/real/testvar/result/', path)
   pdir <- paste0('hca/real/testvar/plot/', path)
@@ -15,21 +20,21 @@ for (path in c('erythroid', 'lymph', 'monocyte')){
   ## ---------------
   Res <- readRDS(paste0(ddir, '/age/age_res.rds'))
   res = Res$statistics
-  ## identify DEGType 
+  ## identify DEGType
   DEGType <- getDEGType(Res)
-  res <- cbind(res, DEGType = DEGType[rownames(res)])
-  saveRDS(res, paste0(rdir, '/age_fdr_res.rds'))
-  write.csv(res, paste0(rdir, '/age_fdr_res.csv'))
+  res <- cbind(res, DEGType = DEGType[rownames(res)], TrurPositive = ifelse(sub(':.*','', rownames(res)) %in% agediff, 'TRUE', 'FALSE'))
+  saveRDS(res, paste0(rdir, '/age/age_fdr_res.rds'))
+  write.csv(res, paste0(rdir, '/age/age_fdr_res.csv'))
   sink(paste0(rdir, '/age/age_DEGType.txt'))
   table(DEGType)
   sink()
-  
+}
   ## plot DEG: all, meanSig, trendSig
   res <- res[order(res[,7], -res[,8]), ]
   png(paste0(pdir, '/age/age_diffgene.png'), width = 1700, height = 1500, res = 200)
   plotGenePopulation(Res, rownames(res)[1:min(25, nrow(res))], type = 'variable', sep = ':.*')
   dev.off()
-  
+
   res <- res[order(res[,1], -res[,2]), ]
   int <- intersect(rownames(res)[res[,1]<0.05], names(DEGType)[DEGType == 'meanSig'])
   png(paste0(pdir, '/age/age_diffgene_meanSig.png'), width = 1700, height = 1500, res = 200)
@@ -43,18 +48,20 @@ for (path in c('erythroid', 'lymph', 'monocyte')){
     plotGenePopulation(Res, int[1:min(25, length(int))], type = 'variable', sep = ':.*')
     dev.off()
   }
-    
+
   ### ------------------
   ### gender difference
   ### ------------------
   Res <- readRDS(paste0(ddir, '/gender/gender_res.rds'))
   res <- Res$statistics
   DEGType <- getDEGType(Res)
-  res <- cbind(res, DEGType = DEGType[rownames(res)])
+  res <- cbind(res, DEGType = DEGType[rownames(res)], 
+               inChrX = ifelse(sub(':.*', '', rownames(res)) %in% ChrX, 'TRUE', 'FALSE'),
+               inChrY = ifelse(sub(':.*', '', rownames(res)) %in% ChrY, 'TRUE', 'FALSE'))
   saveRDS(res, paste0(rdir, '/gender/gender_fdr_res.rds'))
   write.csv(res, paste0(rdir, '/gender/gender_fdr_res.csv'))
-  
-  sink(paste0(rdir, '/gender_DEGType.txt'))
+
+  sink(paste0(rdir, '/gender/gender_DEGType.txt'))
   table(DEGType)
   sink()
   
