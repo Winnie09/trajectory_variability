0014406197a93d826fe8414f8ed7829730b843fb
Wenpin Hou <wp.hou3@gmail.com>
2021-02-16 07:10:36 -0500

    09_age_diff.R

--- hca/real/testvar/code/09_age_diff.R
+++ hca/real/testvar/code/09_age_diff.R
@@ -1,74 +1,62 @@
 library(here)
-setwd(here())
+setwd('/Users/wenpinhou/Dropbox/trajectory_variability')
+# setwd(here())
 ddir <- rdir <- 'hca/real/testvar/result/'
 pdir <- 'hca/real/testvar/plot/'
 u1 <- readRDS('agediff/result/age_diffgene.rds')
 source('function/01_function.R')
 res.gtex <- readRDS('agediff/result/res.rds')
 
-
 for (path in c('monocyte', 'lymph', 'erythroid')){
   print(path)
-  
-  res <- readRDS(paste0('hca/real/testvar/result/', path, '/age_fdr_res.rds'))
-  res <- res[order(res[,1], -abs(res[,3])), ]
+  res <- readRDS(paste0('hca/real/testvar/result/', path, '/age/age_fdr_res.rds'))
+  res <- res[order(res[,7], -abs(res[,8])), ]
   rn <- sub(':.*', '', rownames(res))
   res <- res[!duplicated(rn), ]
   rownames(res) <- rn[!duplicated(rn)]
   res <- res[rownames(res) %in% rownames(res.gtex), ]
   
-  res.lm <- readRDS(paste0('hca/real/testvar/result/', path, '/meandiff_age_res.rds'))
+  res.lm <- readRDS(paste0('hca/real/testvar/result/', path, '/age/meandiff_age_res.rds'))
   res.lm <- res.lm[order(res.lm[,5], -abs(res.lm[,1])), ]
   rn <- sub(':.*', '', rownames(res.lm))
   res.lm <- res.lm[!duplicated(rn), ]
   rownames(res.lm) <- rn[!duplicated(rn)]
   res.lm <- res.lm[rownames(res.lm) %in% rownames(res.gtex), ]
   
+  ## our method overlap
+  allg <- rownames(res)
+  
+  targetgene <- allg
+  v1 <- cumsum(targetgene %in% intersect(targetgene,u1))/seq(1:length(targetgene))
+  v1 <- v1[seq(1,length(v1)) %% 10 == 0]
+  saveRDS(mean(v1), paste0(rdir, path, '/age/age_overlap.rds'))
   
   ## limmma overlap
   targetgene2 <- rownames(res.lm)
   v2 <- cumsum(targetgene2 %in% intersect(targetgene2,u1))/seq(1:length(targetgene2))
   v2 <- v2[seq(1,length(v2)) %% 10 == 0]
-  saveRDS(mean(v2), paste0(rdir, path, '/meandiff_age_overlap.rds'))
-  
-    
-  ## our method overlap
-  allg <- rownames(res)
-  diffgene = rownames(res[res[,1] < 0.05,])
-  str(diffgene)
-
-  targetgene <- allg
-  v1 <- cumsum(targetgene %in% intersect(targetgene,u1))/seq(1:length(targetgene))
-  v1 <- v1[seq(1,length(v1)) %% 10 == 0]
-  saveRDS(mean(v1), paste0(rdir, path, '/age_overlap.rds'))
+  saveRDS(mean(v2), paste0(rdir, path, '/age/meandiff_age_overlap.rds'))
   
   v1_pm <- sapply(seq(1,1e4), function(myseed){
     set.seed(myseed)
-    #w1 = sample(targetgene, length(intersect(targetgene,u1)))
-    #k1 <- cumsum(targetgene %in% w1)/seq(1:length(targetgene))
-    
     sampgene <- sample(targetgene)
     k1 <- cumsum(sampgene %in% intersect(sampgene,u1))/seq(1:length(sampgene))
-    
     k1 <- k1[seq(1,length(k1)) %% 10 == 0]
   })
   summary(colMeans(v1_pm))
   rownames(v1_pm) <- paste0('top',seq(1,nrow(v1_pm)))
   pval <- mean(v1_pm > mean(v1))
   
-  
-  
-  
   pd <- data.frame(agediff_pm = colMeans(v1_pm), stringsAsFactors = FALSE)
   library(ggplot2)
-  pdf(paste0(pdir, path, '/agediff_hist_mean_overlap_proportion.pdf'), width = 3.5, height = 3)
+  pdf(paste0(pdir, path, '/age/agediff_hist_mean_overlap_proportion.pdf'), width = 2.8, height = 2.5)
   print(ggplot(data = pd, aes(x=agediff_pm)) + 
    geom_histogram(aes(y=..density..), colour="black", fill="white")+
    geom_density(alpha=.2, fill="#FF6666") +
    geom_vline(xintercept = mean(v1), color = 'red') +
     theme_classic()+
     xlab('overlap proportion mean')+
-    ggtitle(paste0('age differential, p.value = ', pval)))
+    ggtitle(paste0('age, p.value = ', round(pval,3))))
   dev.off()
   
   
@@ -79,7 +67,7 @@ for (path in c('monocyte', 'lymph', 'erythroid')){
   }
   library(ggplot2)
   library(RColorBrewer)
-  pdf(paste0(pdir, path, '/agediff_curve_number.pdf'), width=3.5, height=3)
+  pdf(paste0(pdir, path, '/age/agediff_curve_number.pdf'), width=3.5, height=2.2)
   print(ggplot(mat,aes(x=order,y=v,col=type, fill=type), alpha=.2) + 
     geom_line() + 
     theme_classic()+
@@ -88,19 +76,18 @@ for (path in c('monocyte', 'lymph', 'erythroid')){
     scale_color_manual(values = brewer.pal(14, 'Paired')[c(2,8)]))
   dev.off()
   
-  Res <- readRDS(paste0(ddir, path, '/age_res.rds'))
-  pdf(paste0(pdir, path, '/agediff_true_genes.pdf'), width=12, height=12)
-  res <- readRDS(paste0('hca/real/testvar/result/', path, '/age_fdr_res.rds'))
-  res <- res[order(res[,1], -abs(res[,3])), ]
-  allg.full <- rownames(res[res[,1] < 0.05,])
-  allg.full <- allg.full[allg %in% u1]
-  plotGenePopulation(Res, allg.full[1:min(length(allg.full), 25)], type = 'variable')
-  dev.off()
   
+  diffgene <- rownames(res[res[,7] < 0.05, ])
+  str(diffgene)
+  if (sum(diffgene%in% u1) > 0){
+    Res <- readRDS(paste0(ddir, path, '/age/age_res.rds'))
+    pdf(paste0(pdir, path, '/age/agediff_true_genes.pdf'), width=12, height=12)
+    res <- readRDS(paste0('hca/real/testvar/result/', path, '/age/age_fdr_res.rds'))
+    res <- res[order(res[,7], -abs(res[,8])), ]
+    int <- intersect(rownames(res[res[,11] == 'TRUE', ]), rownames(res[res[,7] < 0.05, ]))
+    plotGenePopulation(Res, int[1:min(length(int), 25)], type = 'variable')
+    dev.off()
+  }
 }
-
-
-
-
 
 
