28d3f8145f89814d378e2db4d0e3028b5f200fc9
Wenpin Hou <wp.hou3@gmail.com>
2021-03-09 15:18:10 -0500

    adapt to the new output from NIW models:both chisq and pm

--- function/getDEGType.R
+++ function/getDEGType.R
@@ -1,19 +1,45 @@
-getDEGType <- function(testobj, cutoff = 0.05){
+getDEGType <- function(testobj, cutoff = 0.05, test.type = 'variable', test.method = 'permutation'){
   res <- testobj$statistics
-  diffType <- sapply(rownames(res), function(g){
-    if (res[g, 7] < cutoff){
-      if (res[g, 1] < cutoff & res[g, 4] > cutoff){
-        'meanSig'
-      } else if (res[g, 1] > cutoff & res[g, 4] < cutoff){
-        'trendSig'
-      } else if (res[g, 1] < cutoff & res[g, 4] < cutoff){
-        'bothSig'
-      } else {
-        'other'
-      }
-    } else {
-      'nonDEG'
+  if (test.type == 'variable'){
+    if (test.method == 'permutation'){
+      diffType <- sapply(rownames(res), function(g){
+        if (res[g, 1] < cutoff){
+          if (res[g, 4] < cutoff & res[g, 7] > cutoff){
+            'meanSig'
+          } else if (res[g, 4] > cutoff & res[g, 7] < cutoff){
+            'trendSig'
+          } else if (res[g, 4] < cutoff & res[g, 7] < cutoff){
+            'bothSig'
+          } else {
+            'other'
+          }
+        } else {
+          'nonDEG'
+        }
+      })
+    } else if (test.method == 'chisq'){
+      diffType <- sapply(rownames(res), function(g){
+        if (res[g, 1] < cutoff){
+          if (res[g, 5] < cutoff & res[g, 3] > cutoff){
+            'meanSig'
+          } else if (res[g, 5] > cutoff & res[g, 3] < cutoff){
+            'trendSig'
+          } else if (res[g, 5] < cutoff & res[g, 3] < cutoff){
+            'bothSig'
+          } else {
+            'other'
+          }
+        } else {
+          'nonDEG'
+        }
+      })
     }
-  })
+    
+  } else if (test.type == 'time'){
+    print('work on it now!')
+  }
+  
   return(diffType)
 }
+
+
