956b07ae767193e64141da202282050cdeeabfd5
Wenpin Hou <wp.hou3@gmail.com>
2021-03-09 14:31:48 -0500

    add chisq to trendMean

--- hca/simu/testvar/addMultiSignalUsingExpr/code/03.3_perf_trendMean.R
+++ hca/simu/testvar/addMultiSignalUsingExpr/code/03.3_perf_trendMean.R
@@ -1,7 +1,7 @@
 rm(list=ls())
+#setwd('/Users/wenpinhou/Dropbox/trajectory_variability')
 library(here)
 setwd(here())
-# setwd('/Users/wenpinhou/Dropbox/trajectory_variability')
 ddir <- 'hca/simu/testvar/addMultiSignalUsingExpr/result/'
 rdir <- 'hca/simu/testvar/addMultiSignalUsingExpr/result/perf/'
 selgene1 <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/data/selgene/selgene1.rds')
@@ -9,6 +9,7 @@ selgene2 <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/data/selgene/selge
 selgene3 <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/data/selgene/selgene3.rds')
 selgene = selgene3
 rmgene = c(selgene1, selgene2)
+
 str(selgene)
 str(rmgene)
 source('function/01_function.R')
@@ -19,13 +20,28 @@ af = list.files(paste0(ddir, m, '/'))
 df1.nc <- sapply(af, function(f){
   print(f)
   r = readRDS(paste0(ddir, m, '/', f))
-  res = r$statistics[, 7:9]
-  colnames(res) <- c('fdr', 'fc', 'pval')
+  res = r$statistics
+  res = res[order(res[,1], -res[,2]), c(1:3)]
+  colnames(res) <- c('fdr', 'zscore', 'pvalue')
   res = res[!rownames(res) %in% rmgene, ]
-  res = res[order(res[,1], -res[,2]), ]
-  c(sub('.rds','',f), 'EM_NOT_centered', AreaUnderSensFdr(SensFdr(selgene, res)))
+  c(sub('.rds','',f), 'EM_pm', AreaUnderSensFdr(SensFdr(selgene, res)))
 })
-perflist[['EM_not_centered']] = t(df1.nc)
+perflist[['EM_pm']] = t(df1.nc)
+
+
+m = 'chisq'
+af = list.files(paste0(ddir, m, '/'))
+df1 <- sapply(af, function(f){
+  print(f)
+  r = readRDS(paste0(ddir, m, '/', f))
+  res = r$statistics
+  res = res[order(res[,1], res[,2]), c(1:2)]
+  colnames(res) <- c('fdr', 'pvalue')
+  res = res[!rownames(res) %in% rmgene, ]
+  c(sub('.rds','',f), 'EM_chisq', AreaUnderSensFdr(SensFdr(selgene, res)))
+})
+perflist[['EM_chisq']] = t(df1)
+
 
 m = 'meandiff'
 af = list.files(paste0(ddir, m, '/'))
@@ -34,9 +50,10 @@ df2 <- sapply(af, function(f){
   res = readRDS(paste0(ddir, m, '/', f))
   res = res[!rownames(res) %in% rmgene, ]
   res = res[order(res[,5], -abs(res[,1])), ]
-  c(sub('.rds','',f), 'limma_pb', AreaUnderSensFdr(SensFdr(selgene, res)))
+  c(sub('.rds','',f), 'limma', AreaUnderSensFdr(SensFdr(selgene, res)))
 })
-perflist[['limma_pb']] = t(df2)
+perflist[['limma']] = t(df2)
+
 
 m = 'tscan'
 af = list.files(paste0(ddir, m, '/'))
@@ -53,8 +70,12 @@ saveRDS(perflist, paste0(rdir,'perflist_trendMean.rds'))
 perf <- do.call(rbind, perflist)
 colnames(perf) <- c('SignalStrength', 'Method', 'Fdr.Diff', 'AUC')
 perf <- as.data.frame(perf)
+
+perf[,1] <- as.numeric(as.character(perf[,1]))
+perf[,3] <- as.numeric(as.character(perf[,3]))
+perf[,4] <- as.numeric(as.character(perf[,4]))
+
 saveRDS(perf, paste0(rdir,'perf_trendMean.rds'))
 rm(list=ls())
-
 
 
