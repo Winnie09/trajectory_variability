81c8e849de8a577a0897c99c1b04fbd652bfbf41
Wenpin Hou <wp.hou3@gmail.com>
2021-03-09 20:02:40 -0500

    add method EM_pm

--- covid/Su_2020_Cell/testvar/useSaverImp/code/01_numeric_HD_others.R
+++ covid/Su_2020_Cell/testvar/useSaverImp/code/01_numeric_HD_others.R
@@ -1,3 +1,6 @@
+m <- as.character(commandArgs(trailingOnly = T)[[1]])
+print(m)
+
 library(here)
 setwd(here())
 expr <- readRDS('covid/Su_2020_Cell/data/saver_log2norm_sub.rds')
@@ -9,9 +12,12 @@ expr <- expr[rowMeans(expr>0.1)>0.01, ]
 expr <- expr[, cellanno[,1]]
 
 source('function/01_function.R')
-rdir <- 'covid/Su_2020_Cell/testvar/useSaverImp/result/'
+rdir <- paste0('covid/Su_2020_Cell/testvar/useSaverImp/result/', m, '/HD_others/')
+dir.create(rdir, showWarnings = F, recursive = T)
 system.time({
-  res <- testpt.seed(expr=expr, cellanno=cellanno, pseudotime=pt, design=design, type='Variable', ncores = 8, demean = FALSE)
+  res <- testpt(expr=expr, cellanno=cellanno, pseudotime=pt, design=design, test.type='Variable', ncores = 12, demean = FALSE, test.method = ifelse(m == 'EM_pm', 'permutation', 'chisq'))
 })
-saveRDS(res, paste0(rdir, 'numeric_HD_others_res.rds'))
+saveRDS(res, paste0(rdir, 'numeric_res.rds'))
+
+
 
