c81bb577105554068ba115f116eec779f9f2eab7
Wenpin Hou <wp.hou3@gmail.com>
2021-03-09 20:02:44 -0500

    add method EM_pm

--- covid/Su_2020_Cell/testvar/useSaverImp/code/02_numeric_4levels.R
+++ covid/Su_2020_Cell/testvar/useSaverImp/code/02_numeric_4levels.R
@@ -1,3 +1,8 @@
+m <- as.character(commandArgs(trailingOnly = T)[[1]])
+print(m)
+i = commandArgs(trailingOnly = T)[[2]]
+print(i)
+
 library(here)
 setwd(here())
 expr <- readRDS('covid/Su_2020_Cell/data/saver_log2norm_sub.rds')
@@ -10,21 +15,19 @@ design <- readRDS('covid/Su_2020_Cell/data/design_numeric_4levels.rds') ##
 expr <- expr[rowMeans(expr>0.1)>0.01, ]
 expr <- expr[, cellanno[,1]]
 source('function/01_function.R')
-rdir <- 'covid/Su_2020_Cell/testvar/useSaverImp/result/'
-i = commandArgs(trailingOnly = T)[[1]][1]
+rdir <- paste0('covid/Su_2020_Cell/testvar/useSaverImp/result/', m, '/4levels/')
+dir.create(rdir, showWarnings = F, recursive = T)
+
 if (i == '1'){
   system.time({
-    res <- testpt(expr=expr[1:4000, ], cellanno=cellanno, pseudotime=pt, design=design, type='Variable', ncores = 20, demean = FALSE)
+    res <- testpt(expr=expr[1:4000, ], cellanno=cellanno, pseudotime=pt, design=design, test.type='Variable', ncores = 20, demean = FALSE, test.method = ifelse(m == 'EM_pm', 'permutation', 'chisq'))
   })
-  saveRDS(res, paste0(rdir, 'numeric_4levels_res_1.rds'))
+  saveRDS(res, paste0(rdir, 'numeric_res_1.rds'))
 } else {
   system.time({
-    res <- testpt(expr=expr[4001:nrow(expr), ], cellanno=cellanno, pseudotime=pt, design=design, type='Variable', ncores = 20, demean = FALSE)
+    res <- testpt(expr=expr[4001:nrow(expr), ], cellanno=cellanno, pseudotime=pt, design=design, test.type='Variable', ncores = 20, demean = FALSE, test.method = ifelse(m == 'EM_pm', 'permutation', 'chisq'))
   })
-  saveRDS(res, paste0(rdir, 'numeric_4levels_res_2.rds'))
+  saveRDS(res, paste0(rdir, 'numeric_res_2.rds'))
 }
-
-
-  
 
 
