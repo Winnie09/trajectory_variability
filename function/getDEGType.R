bd0a88b6fef3f034828349de8e386150ecc20d40
Wenpin Hou <wp.hou3@gmail.com>
2021-03-11 11:07:13 -0500

    adapt to new NIW return data structures

--- function/getDEGType.R
+++ function/getDEGType.R
@@ -1,44 +1,24 @@
-getDEGType <- function(testobj, cutoff = 0.05, test.type = 'variable', test.method = 'permutation'){
+getDEGType <- function(testobj, cutoff = 0.05){
   res <- testobj$statistics
-  if (test.type == 'variable'){
-    if (test.method == 'permutation'){
-      diffType <- sapply(rownames(res), function(g){
-        if (res[g, 1] < cutoff){
-          if (res[g, 4] < cutoff & res[g, 7] > cutoff){
-            'meanSig'
-          } else if (res[g, 4] > cutoff & res[g, 7] < cutoff){
-            'trendSig'
-          } else if (res[g, 4] < cutoff & res[g, 7] < cutoff){
-            'bothSig'
-          } else {
-            'other'
-          }
+  if (testobj$test.type == 'Variable' | testobj$test.type == 'variable'){
+    diffType <- sapply(rownames(res), function(g){
+      if (res[g, grep('^fdr.*overall$', colnames(res))] < cutoff){
+        if (res[g, grep('^fdr.*meanDiff$', colnames(res))] < cutoff & res[g, grep('^fdr.*trendDiff$', colnames(res))] > cutoff){
+          'meanSig'
+        } else if (res[g, grep('^fdr.*meanDiff$', colnames(res))] > cutoff & res[g, grep('^fdr.*trendDiff$', colnames(res))] < cutoff){
+          'trendSig'
+        } else if (res[g, grep('^fdr.*meanDiff$', colnames(res))] < cutoff & res[g, grep('^fdr.*trendDiff$', colnames(res))] < cutoff){
+          'bothSig'
         } else {
-          'nonDEG'
+          'other'
         }
-      })
-    } else if (test.method == 'chisq'){
-      diffType <- sapply(rownames(res), function(g){
-        if (res[g, 1] < cutoff){
-          if (res[g, 5] < cutoff & res[g, 3] > cutoff){
-            'meanSig'
-          } else if (res[g, 5] > cutoff & res[g, 3] < cutoff){
-            'trendSig'
-          } else if (res[g, 5] < cutoff & res[g, 3] < cutoff){
-            'bothSig'
-          } else {
-            'other'
-          }
-        } else {
-          'nonDEG'
-        }
-      })
-    }
-    
-  } else if (test.type == 'time'){
+      } else {
+        'nonDEG'
+      }
+    })
+  } else if (testobj$test.type == 'Time' | testobj$test.type == 'time'){
     print('work on it now!')
   }
-  
   return(diffType)
 }
 
