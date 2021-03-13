4807d1dd90ee2333477f2782f9666997ab4870aa
Wenpin Hou <wp.hou3@gmail.com>
2021-03-13 15:39:09 -0500

    10_meandiff.R save to limma directory

--- hca/real/testvar/code/10_meandiff.R
+++ hca/real/testvar/code/10_meandiff.R
@@ -4,8 +4,9 @@ source('./function/01_function.R')
 for (path in c('lymph/', 'erythroid/', 'monocyte/')){
   print(path)
   ddir <- paste0('hca/real/build3traj/manual/result/', path)
-  rdir <- paste0('hca/real/testvar/result/', path)
+  rdir <- paste0('hca/real/testvar/result/limma/', path)
   dir.create(rdir, recursive = TRUE, showWarnings = FALSE)
+  print(rdir)
   
   m = readRDS(paste0(ddir, 'input_expr.rds'))
   cellanno = readRDS(paste0(ddir, 'input_cellanno.rds'))
@@ -14,17 +15,21 @@ for (path in c('lymph/', 'erythroid/', 'monocyte/')){
   
   design = design.bak[, 1:2]
   design[,2] <- ifelse(design[,2] == 'male', 0, 1)
-  
+  print(design)  
   res <- meandiff(expr = m, cellanno = cellanno, design = design)
   res <- res[order(res[,'adj.P.Val'], -abs(res[, 'logFC'])), ]
-  saveRDS(res, paste0(rdir, 'meandiff_gender_res.rds'))
+  print(sum(res[,'adj.P.Val']<0,05))
+  saveRDS(res, paste0(rdir, 'gender_res.rds'))
   
   design = design.bak[, c(1,3)]
   design[,2] <- as.numeric(design[,2])
+  print(design)
   res <- meandiff(expr = m, cellanno = cellanno, design = design)
   res <- res[order(res[,'adj.P.Val'], -abs(res[, 'logFC'])), ]
-  saveRDS(res, paste0(rdir, 'meandiff_age_res.rds'))
+  print(sum(res[,'adj.P.Val']<0,05))
+  saveRDS(res, paste0(rdir, 'age_res.rds'))
 }
   
+
 
 
