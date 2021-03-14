e8fd9ee1f21acf2a3c366b3d7fb1bbf1000f420a
Wenpin Hou <wp.hou3@gmail.com>
2021-03-13 23:00:58 -0500

    14_multiDEGType_check.R

---
+++ hca/real/testvar/code/15_compare_all_methods_numDEG.R
@@ -0,0 +1,50 @@
+setwd('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testvar/result/EM_chisq')
+numDEG <- sapply(c('lymph', 'erythroid', 'monocyte'), function(path){
+  print(path)
+  sapply(c('gender_res.rds', 'age_res.rds'), function(fn){
+    print(fn)
+    res = readRDS(paste0(path, '/', fn))
+    s = res$statistics
+    sum(s[, grepl('^fdr.*overall$', colnames(s))] < 0.05)
+  })
+  
+})
+    
+
+
+setwd('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testvar/result/EM_pm')
+numDEG <- sapply(c('lymph', 'erythroid', 'monocyte'), function(path){
+  print(path)
+  sapply(c('gender_res.rds', 'age_res.rds'), function(fn){
+    print(fn)
+    if (file.exists(paste0(path, '/', fn))){
+      res = readRDS(paste0(path, '/', fn))
+      s = res$statistics
+      sum(s[, grepl('^fdr.*overall$', colnames(s))] < 0.05)
+    } else {
+      NA
+    }
+      
+  })
+  
+})
+    
+
+
+setwd('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testvar_not_NIW/result/')
+numDEG <- sapply(c('lymph', 'erythroid', 'monocyte'), function(path){
+  print(path)
+  sapply(c('meandiff_gender_res.rds', 'meandiff_age_res.rds'), function(fn){
+    print(fn)
+    if (file.exists(paste0(path, '/', fn))){
+      res = readRDS(paste0(path, '/', fn))
+      s = res$statistics
+      sum(s[, grepl('adj', colnames(s))] < 0.05)
+    } else {
+      NA
+    }
+      
+  })
+  
+})
+
