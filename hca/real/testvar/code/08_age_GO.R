1572c4ce6ffdf35fdd0954d69e7f4b98d1194f03
Wenpin Hou <wp.hou3@gmail.com>
2021-03-16 10:04:23 -0400

    adapt to new result structure

--- hca/real/testvar/code/08_age_GO.R
+++ hca/real/testvar/code/08_age_GO.R
@@ -1,20 +1,20 @@
 library(here)
 setwd(here())
-ddir <- 'hca/real/testvar/result/'
+ddir <- 'hca/real/testvar/result/EM_pm/'
 path = 'erythroid'
+source('/home-4/whou10@jhu.edu/scratch/Wenpin/resource/myfunc/01_function.R')
+source('function/01_function.R')
+
 for (path in c('monocyte', 'lymph', 'erythroid')){
   print(path)
-  res <- readRDS(paste0(ddir, path, '/age_fdr_res.rds'))
-  source('/home-4/whou10@jhu.edu/scratch/Wenpin/resource/myfunc/01_function.R')
-  rn <- sub(':.*', '', rownames(res))
-  res <- res[!duplicated(rn), ]
-  rownames(res) <- sub(':.*', '', rownames(res))
-  print(dim(res[res[,1]<0.05, ]))
-  print(rownames(res[res[,1]<0.05, ])[1:100])
-  go <- myGO(rownames(res[res[,1]<0.05, ]), rownames(res))
-  go <- go[order(go[,'FDR'], -abs(go[, 'FC'])), ]
-  print(summary(go[, 'FDR']))
-  print(head(go))
+  Res <- readRDS(paste0(ddir, path, '/age/age_res.rds'))
+  go <- GOEnrich(Res, use.clusters = F)
+  go <- go[[1]]
+  go <- go[go[,'FDR']<0.05, ,drop=F]
+  if (nrow(go) > 0){
+    write.csv(go, paste0(ddir, path, 'age/age_res_GO.csv'))
+  }
 }
 
   
+
