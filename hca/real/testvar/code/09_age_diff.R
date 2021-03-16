bcded9278962ba7a806cab4d2be5b07fa344407b
Wenpin Hou <wp.hou3@gmail.com>
2021-03-16 11:50:13 -0400

    09_age_diff.R:adapt to new result structure

--- hca/real/testvar/code/09_age_diff.R
+++ hca/real/testvar/code/09_age_diff.R
@@ -1,22 +1,24 @@
 library(here)
-setwd('/Users/wenpinhou/Dropbox/trajectory_variability')
-# setwd(here())
-ddir <- rdir <- 'hca/real/testvar/result/'
-pdir <- 'hca/real/testvar/plot/'
+# setwd('/Users/wenpinhou/Dropbox/trajectory_variability')
+setwd(here())
+ddir <- rdir <- 'hca/real/testvar/result/EM_pm/'
+pdir <- 'hca/real/testvar/plot/EM_pm/'
 u1 <- readRDS('agediff/result/age_diffgene.rds')
 source('function/01_function.R')
 res.gtex <- readRDS('agediff/result/res.rds')
 
 for (path in c('monocyte', 'lymph', 'erythroid')){
   print(path)
-  res <- readRDS(paste0('hca/real/testvar/result/', path, '/age/age_fdr_res.rds'))
-  res <- res[order(res[,7], -abs(res[,8])), ]
+  res <- readRDS(paste0(rdir, path, '/age/age_fdr_res.rds'))
+  res <- res[,1:9]
+  res <- matrix(as.numeric(res), ncol=9, dimnames = dimnames(res))
+  res <- res[order(res[,1], -res[,3]), ]
   rn <- sub(':.*', '', rownames(res))
   res <- res[!duplicated(rn), ]
   rownames(res) <- rn[!duplicated(rn)]
   res <- res[rownames(res) %in% rownames(res.gtex), ]
   
-  res.lm <- readRDS(paste0('hca/real/testvar/result/', path, '/age/meandiff_age_res.rds'))
+  res.lm <- readRDS(paste0('hca/real/testvar/result/limma/', path, '/age_res.rds'))
   res.lm <- res.lm[order(res.lm[,5], -abs(res.lm[,1])), ]
   rn <- sub(':.*', '', rownames(res.lm))
   res.lm <- res.lm[!duplicated(rn), ]
@@ -35,7 +37,7 @@ for (path in c('monocyte', 'lymph', 'erythroid')){
   targetgene2 <- rownames(res.lm)
   v2 <- cumsum(targetgene2 %in% intersect(targetgene2,u1))/seq(1:length(targetgene2))
   v2 <- v2[seq(1,length(v2)) %% 10 == 0]
-  saveRDS(mean(v2), paste0(rdir, path, '/age/meandiff_age_overlap.rds'))
+  saveRDS(mean(v2), paste0(rdir, path, '/age/limma_age_overlap.rds'))
   
   v1_pm <- sapply(seq(1,1e4), function(myseed){
     set.seed(myseed)
@@ -77,17 +79,21 @@ for (path in c('monocyte', 'lymph', 'erythroid')){
   dev.off()
   
   
-  diffgene <- rownames(res[res[,7] < 0.05, ])
+  diffgene <- rownames(res[res[,1] < 0.05, ])
   str(diffgene)
   if (sum(diffgene%in% u1) > 0){
     Res <- readRDS(paste0(ddir, path, '/age/age_res.rds'))
-    pdf(paste0(pdir, path, '/age/agediff_true_genes.pdf'), width=12, height=12)
-    res <- readRDS(paste0('hca/real/testvar/result/', path, '/age/age_fdr_res.rds'))
-    res <- res[order(res[,7], -abs(res[,8])), ]
-    int <- intersect(rownames(res[res[,11] == 'TRUE', ]), rownames(res[res[,7] < 0.05, ]))
-    plotGenePopulation(Res, int[1:min(length(int), 25)], type = 'variable')
-    dev.off()
+    
+    res <- readRDS(paste0(ddir, path, '/age/age_fdr_res.rds'))
+    res <- res[order(as.numeric(res[,1]), -as.numeric(res[,3])), ]
+    int <- intersect(rownames(res[res[,11] == 'TRUE', ]), rownames(res[res[,1] < 0.05, ]))
+    if (!is.na(int) & length(int) > 0){
+      pdf(paste0(pdir, path, '/age/agediff_true_genes.pdf'), width=12, height=12)
+      print(plotGenePopulation(Res, int[1:min(length(int), 25)], type = 'variable'))
+      dev.off()
+    }
   }
 }
+
 
 
