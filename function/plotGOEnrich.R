37d6bb0ebb3cc19e845e2833dbeb3dbe60a787e0
Wenpin Hou <wp.hou3@gmail.com>
2021-03-18 13:40:10 -0400

    sort hm rows by cluster

--- function/plotGOEnrich.R
+++ function/plotGOEnrich.R
@@ -25,10 +25,14 @@ plotGOEnrich <- function(goRes, n = 5){
   dmat <- as.matrix(dmat[,-1,drop=F])
   dmat <- is.na(dmat)
   
+  v <- sapply(1:nrow(dmat), function(i) which.min(dmat[i,]))
+  names(v) <- rownames(dmat)
+  
   pd <- melt(dmat)
   colnames(pd) <- c('Term','Cluster','enc')
-  pd$Term <- factor(pd$Term,levels=rownames(dmat)[hclust(dist(dmat))$order])
-  pd$enc <- ifelse(pd$enc,'Non-significant','Significant')
+  # pd$Term <- factor(as.character(pd$Term),levels=rownames(dmat)[hclust(dist(dmat))$order])
+ pd$Term <- factor(as.character(pd$Term),levels=names(v[rev(order(v))]))
+   pd$enc <- ifelse(pd$enc,'Non-significant','Significant')
   p <- ggplot(pd,aes(x=Cluster,y=Term,fill=enc)) + geom_tile() + theme_classic() + scale_fill_manual(values=c('darkblue', 'orange'))+
     theme(legend.position = 'right')+
     scale_y_discrete(position = "right")
