7ab4794e7f09d9080b5f8d657abc332bde1ad5b7
Wenpin Hou <wp.hou3@gmail.com>
2021-03-30 15:22:06 -0400

    tscan use two branch fitting residues to rank genes

--- function/TSCAN.R
+++ function/TSCAN.R
@@ -1,33 +1,35 @@
 library(mgcv)
 library(parallel)
-
 TSCAN_group <- function(expr,pseudotime,branch, parallel = FALSE, n.cores = 8) {
- # expr: saver imputed matrix
- # count: count matrix
- # pseudotime: numeric vector (1,2,3,4....) with names same as colnames(expr)
- # branch: 0,1 vector indicating whether each cell is from group 1 or 2, can get from as.numeric(sub(':.*','',colnames(expr)) %in% paste0('BM',c(1,2,5,6)))
- # cell_coords: the pca you sent me, only use the first 4 (if correct) dimensions
+  # expr: saver imputed matrix
+  # count: count matrix
+  # pseudotime: numeric vector (1,2,3,4....) with names same as colnames(expr)
+  # branch: 0,1 vector indicating whether each cell is from group 1 or 2, can get from as.numeric(sub(':.*','',colnames(expr)) %in% paste0('BM',c(1,2,5,6)))
+  # cell_coords: the pca you sent me, only use the first 4 (if correct) dimensions
   if (parallel){
-    pval <- mclapply(row.names(expr),function(g) {
-      x <- expr[g,]
-      m1 <- mgcv::gam(x~s(pseudotime,k=3,by=branch))
-      m2 <- mgcv::gam(x~s(pseudotime,k=3))
-      c(pchisq(m2$deviance - m1$deviance, m2$df.residual - m1$df.residual,lower.tail = F),m2$deviance - m1$deviance)
+    res <- mclapply(rownames(expr), function(g){
+      x <- expr[g, ]
+      m1 <- mgcv::gam(a~s(pt,k=3),data=data.frame(a=x[branch==1], pt=pseudotime[branch==1]))
+      p1 <- predict(m1, data.frame(pt=pseudotime))
+      m2 <- mgcv::gam(a~s(pt,k=3),data=data.frame(a=x[branch==0], pt=pseudotime[branch==0]))
+      p2 <- predict(m2, data.frame(pt=pseudotime))
+      sum((p1-p2)^2)
     },mc.cores = n.cores)
+    res <- unlist(res)
+    names(res) <- rownames(expr)
   } else {
-    pval <- lapply(row.names(expr),function(g) {
-      x <- expr[g,]
-      m1 <- mgcv::gam(x~s(pseudotime,k=3,by=branch))
-      m2 <- mgcv::gam(x~s(pseudotime,k=3))
-      c(pchisq(m2$deviance - m1$deviance, m2$df.residual - m1$df.residual,lower.tail = F),m2$deviance - m1$deviance)
+    res <- sapply(rownames(expr), function(g){
+      x <- expr[g, ]
+      m1 <- mgcv::gam(a~s(pt,k=3),data=data.frame(a=x[branch==1], pt=pseudotime[branch==1]))
+      p1 <- predict(m1, data.frame(pt=pseudotime))
+      m2 <- mgcv::gam(a~s(pt,k=3),data=data.frame(a=x[branch==0], pt=pseudotime[branch==0]))
+      p2 <- predict(m2, data.frame(pt=pseudotime))
+      sum((p1-p2)^2)
     })
+    return(res)
   }
-  names(pval) <- row.names(expr)
-  res <- do.call(rbind,pval)
-  res <- data.frame(pval=res[,1],chisq=res[,2])
-  res$fdr <- p.adjust(res[,1],method='fdr')
-  res
 }
+
 
 TSCAN_time <- function(expr, pseudotime, parallel = FALSE, n.cores = 8) {
   if (parallel){
@@ -63,4 +65,5 @@ TSCAN_testvar <- function(expr, cellanno, design, pseudotime){
   res <- TSCAN_group(expr, psn, branch)
   return(res)
 }
+
 
