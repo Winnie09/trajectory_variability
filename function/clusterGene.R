49e4fab937a58146570472e7b997f4c5fb0c8228
Wenpin Hou <wp.hou3@gmail.com>
2021-04-15 21:10:40 -0400

    add method = hierarchical

--- function/clusterGene.R
+++ function/clusterGene.R
@@ -2,17 +2,17 @@ cluster_gene <- function(testobj,
                          gene,
                          k,
                          type = 'time',
-                         method = 'kmeans'){
+                         method = 'kmeans', 
+                         scale.difference = F){
   ## testobj: output object from testpt().
   ## gene: a character vector of genes.
-  ## k: number of clusters.
-  ## type: "time" or "variable". A character denoting the population fit is on "time" or "variable".  Default is "time".
-  
+  ## k: number of clusters. 2 are for meanSig, and the remaining k-2 clusters are for DDG of NOT meanSig.
+  ## type: "time" or "variable". A character denoting the population fit is on "time" or "variable".  Default is "time". ## method: 'kmeans'(default) or 'hierarchical'.
   if (type == 'time'){
     if ('populationFit' %in% names(testobj)) {
       fit <- testobj$populationFit
     } else {
-      fit <- getPopulationFit(Res, gene = gene, type = type)
+      fit <- getPopulationFit(testobj, gene = gene, type = type)
     }
   } else if (type == 'variable'){
     if ('covariateGroupDiff' %in% names(testobj)){
@@ -21,11 +21,18 @@ cluster_gene <- function(testobj,
       fit <- getCovariateGroupDiff(testobj = testobj, gene = gene)  
     }
   }
-  mat.scale <- scalematrix(fit[gene, ,drop=F])
+  if (scale.difference){
+    mat.scale <- scalematrix(fit[gene, ,drop=F])
+  } else {
+    max <- apply(abs(fit[gene, ,drop=F]), 1, max)
+    mat.scale <- fit[gene, ,drop=F]/max
+  }
   
   if (method == 'kmeans'){
     set.seed(12345)
     clu <- kmeans(mat.scale, k, iter.max = 1000)$cluster
+  } else if (method == 'hierarchical') {
+    clu <- cutree(hclust(dist(mat.scale)), k = k)
   }
   # order clusters by genes' max expr position
   v <- sapply(unique(clu), function(i) {
@@ -41,7 +48,7 @@ cluster_gene <- function(testobj,
 }
 
 
-clusterGene <- function(testobj, gene, type = 'variable', k=5, method = 'kmeans'){
+clusterGene <- function(testobj, gene, type = 'variable', k=5, method = 'kmeans', scale.difference = F){
   if (toupper(type) == 'TIME'){
     clu <- cluster_gene(testobj = testobj, 
                         gene = gene,
@@ -54,17 +61,17 @@ clusterGene <- function(testobj, gene, type = 'variable', k=5, method = 'kmeans'
     } else {
       DDGType <- getDDGType(testobj)
     }
-    clu <- cluster_gene(testobj, gene = names(DDGType)[!DDGType %in% c('nonDDG', 'meanSig')], type = 'variable', k=k-2)
-    gene = names(DDGType)[!DDGType %in% c('nonDDG', 'meanSig')]
+    clu <- cluster_gene(testobj, gene = names(DDGType)[!DDGType %in% c('nonDDG', 'meanSig')], type = 'variable', k=k-2, scale.difference = scale.difference, method = method)
+    
     
     design = testobj$design
     cellanno = testobj$cellanno
     meandiff <- sapply(c(0,1), function(i){
       as <- rownames(design[design[,2]==i, ])
-      if ('expr' %in% names(Res)){
-        rowMeans(Res$expr[names(DDGType)[DDGType == 'meanSig'], cellanno[cellanno[,2] %in% as,1]])
+      if ('expr' %in% names(testobj)){
+        rowMeans(testobj$expr[names(DDGType)[DDGType == 'meanSig'], cellanno[cellanno[,2] %in% as,1]])
       } else {
-        rowMeans(Res$expr.ori[names(DDGType)[DDGType == 'meanSig'], cellanno[cellanno[,2] %in% as,1]])
+        rowMeans(testobj$expr.ori[names(DDGType)[DDGType == 'meanSig'], cellanno[cellanno[,2] %in% as,1]])
       }
         
     })
@@ -77,6 +84,7 @@ clusterGene <- function(testobj, gene, type = 'variable', k=5, method = 'kmeans'
     names(clu3) <- large1
     clu = c(clu, clu2, clu3)  
   }
+  clu <- clu[gene]
   return(clu)
 }
 
