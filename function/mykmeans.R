366bf009a854704f34751c468e23ffca46d578f3
Wenpin Hou <wp.hou3@gmail.com>
2021-04-17 07:04:46 -0400

    rss cutoff

--- function/mykmeans.R
+++ function/mykmeans.R
@@ -1,23 +1,24 @@
-mykmeans <- function(matrix, number.cluster = NA, maxclunum = 50, seed = 12345){
+mykmeans <- function(matrix, number.cluster = NA, maxclunum = 30, seed = 12345){
   ## cluster the rows
-  set.seed(seed)
   library(parallel)
   if (is.na(number.cluster)){
     rss <- mclapply(1:maxclunum,function(clunum) {
       set.seed(12345)
       tmp <- kmeans(matrix,clunum,iter.max = 1000)
       tmp$betweenss/tmp$totss
-    },mc.cores=20)
+    },mc.cores=30)
     rss <- unlist(rss)
-    x <- 1:maxclunum
-    optclunum <- which.min(sapply(1:maxclunum, function(i) {
-        x2 <- pmax(0, x - i)
-        sum(lm(rss ~ x + x2)$residuals^2)  ## check this
-    }))
-    print(optclunum)
-    clu <- kmeans(matrix,optclunum)
-  } else {
-    clu <- kmeans(matrix, number.cluster)    
+    print(rss)
+    # number.cluster <- which(diff(rss) < 1e-2)[1]
+    x <- 2:maxclunum
+    number.cluster <- x[which.min(sapply(1:length(x), function(i) {
+      x2 <- pmax(0, x - i)
+      sum(lm(rss[-1] ~ x + x2)$residuals^2)  ## check this
+    }))]
   }
-    return(clu)
+  print(number.cluster)
+  set.seed(seed)
+  clu <- kmeans(matrix, number.cluster)    
+  return(clu)
 }
+
