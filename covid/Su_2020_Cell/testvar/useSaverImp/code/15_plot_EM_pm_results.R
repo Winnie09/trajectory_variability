757e925be4163c640182e01d418a4079a2c8463c
Wenpin Hou <wp.hou3@gmail.com>
2021-04-20 21:29:56 -0400

    15_plot_EM_pm_results.R: different go hm, DiffHm3

--- covid/Su_2020_Cell/testvar/useSaverImp/code/15_plot_EM_pm_results.R
+++ covid/Su_2020_Cell/testvar/useSaverImp/code/15_plot_EM_pm_results.R
@@ -3,11 +3,10 @@ library(here)
 setwd(here())
 # setwd('/Users/wenpinhou/Dropbox/trajectory_variability')
 source('function/01_function.R')
-ddir <- 'covid/Su_2020_Cell/testvar/useSaverImp/result/'
 test.method = 'EM_pm'
 # for (comparison in c('HD_Se', 'HD_Mi','Mod_Se','HD_Mod', 'Se_Mi', 'Recovered_Deceased')){
 # for (comparison in list.files(paste0('covid/Su_2020_Cell/testvar/useSaverImp/result/', test.method))){
-for (comparison in c('Se_Mi','Mod_Mi',  'Recovered_Deceased')){
+for (comparison in c('Mod_Mi,','Se_Mi')){ #'Recovered_Deceased', 'HD_Mi', 'HD_Se', 'Mod_Se')){ ## 'Se_Mi', 'Mod_Mi',  
   print(comparison)
   rdir <- paste0('covid/Su_2020_Cell/testvar/useSaverImp/result/', test.method, '/', comparison, '/')
   pdir <- paste0('covid/Su_2020_Cell/testvar/useSaverImp/plot/', test.method, '/', comparison, '/')
@@ -19,91 +18,46 @@ for (comparison in c('Se_Mi','Mod_Mi',  'Recovered_Deceased')){
   statistics = Res$statistics
   diffgene <- rownames(statistics[statistics[, grep('^fdr.*overall$', colnames(statistics))] < 0.05,])
   str(diffgene)
-  ## if the above test works well, then refine the following codes
+  
   ## --------------
   ## population fit
   ## --------------
-  
-  Res$populationFit <- getPopulationFit(Res, gene = diffgene, type = 'variable')
+  Res$populationFit <- getPopulationFit(Res, gene = rownames(Res$expr), type = 'variable')
   
   ## -----------
   ## clustering
   ## -----------
   if (comparison %in% c('Se_Mi', 'Mod_Mi')){
-    Res$covariateGroupDiff <- getCovariateGroupDiff(testobj = Res, gene = diffgene, reverse = T)  
+    Res$covariateGroupDiff <- getCovariateGroupDiff(testobj = Res, gene = rownames(Res$expr), reverse = T)  
   } else {
-    Res$covariateGroupDiff <- getCovariateGroupDiff(testobj = Res, gene = diffgene, reverse = F)
+    Res$covariateGroupDiff <- getCovariateGroupDiff(testobj = Res, gene = rownames(Res$expr), reverse = F)
   }
   
   DDGType <- getDDGType(Res)
+  Res$DDGType <- DDGType
+  
+  ## autoclu
+  # clu <- clusterGene(Res, gene = names(DDGType)[!DDGType %in% c('nonDDG')], type = 'variable', scale.difference = F, method = 'kmeans', k.auto = TRUE)
+  clu <- clusterGene(Res, gene = names(DDGType)[!DDGType %in% c('nonDDG')], type = 'variable', scale.difference = F, method = 'kmeans', k.auto = TRUE) 
   sink(paste0(pdir, '/DDGType_table.txt'))
-  table(DDGType)
+  print(table(DDGType))
+  table(clu)
   sink()
   
-  clu <- clusterGene(Res, gene = names(DDGType)[!DDGType %in% c('nonDDG', 'meanSig')], type = 'variable', k=5)
-  table(clu)
-  # clu2 <- rep(6, sum(DDGType == 'meanSig'))
-  # if (length(clu2) > 0) {
-  #   names(clu2) <- names(DDGType)[DDGType %in% c('meanSig')]
-  #   clu <- c(clu, clu2)
-  # }
-  design = Res$design
-  cellanno = Res$cellanno
-  meandiff <- sapply(c(0,1), function(i){
-    as <- rownames(design[design[,2]==i, ])
-    rowMeans(Res$expr.ori[names(DDGType)[DDGType == 'meanSig'], cellanno[cellanno[,2] %in% as,1]])
-  })
-  
-  large0 <- rownames(meandiff)[meandiff[,1] >= meandiff[,2]]
-  large1 <- rownames(meandiff)[meandiff[,1] < meandiff[,2]]
-  
-  clu2 <- rep(6, length(large0))
-  names(clu2) <- large0
-  clu3 <- rep(7, length(large1))
-  names(clu3) <- large1
-  clu = c(clu, clu2, clu3)
-  Res$cluster <- clu
+  # 47  54 143 161 207 454 249 
+  Res$cluster = clu
   saveRDS(Res, paste0(rdir, paste0('numeric_res_with_clu.rds')))
+  
   ## --------------
   ## save diff gene
   ## --------------
+  gd <- apply(Res$covariateGroupDiff,1, max) - apply(Res$covariateGroupDiff, 1, min)
   allg <- diffgene
-  res <- data.frame(gene = allg, statistics[allg, ], cluster = Res$cluster[allg], stringsAsFactors = F)
-  res <- res[order(res[, grep('^fdr.*overall$', colnames(res))]), ]
+  res <- data.frame(gene = allg, statistics[allg, ],cluster = Res$cluster[allg], 
+                    effect_size = gd[allg], stringsAsFactors = F)
+  res <- res[order(res[,2], -res[,4]), ]
   res <- cbind(res, DDGType = DDGType[rownames(res)])
   write.csv(res, paste0(pdir, 'differential_genes.csv'))
-  
-  ## ----------------
-  ## plotClusterDiff
-  ## ----------------
-  pdf(paste0(pdir, '/cluster_diff.pdf'), width = 3, height = 2)
-  plotClusterDiff(testobj=Res, gene = allg)
-  dev.off()
-  
-  ## ---------------
-  ## plotClusterMean
-  ## ----------------
-  pdf(paste0(pdir, '/cluster_mean.pdf'), width = 5, height = 3.5)
-  plotClusterMean(testobj=Res, cluster = Res$cluster, type = 'variable')
-  dev.off()
-  
-  ## -----------
-  ## GO analysis
-  ## -----------
-  goRes <- GOEnrich(testobj = Res, type = 'variable')  
-  saveRDS(goRes, paste0(pdir, '/goRes.rds'))
-  
-  nn <- sapply(1:length(goRes), function(i){
-    tmp <- goRes[[i]]
-    tmp <- tmp[tmp[, 'FDR'] < 0.05, ]
-    if (nrow(tmp) > 0) {write.csv(tmp, paste0(pdir, 'cluster', i, '_GO.csv'))}
-    print(str(tmp))
-    return(0)
-  })
-  
-  pdf(paste0(pdir, '/hm_GO_term.pdf'), width = 7.2, height = 3.5)
-  print(plotGOEnrich(goRes))
-  dev.off()
   
   ## -----------------------
   ## plotClusterMeanAndDiff
@@ -112,33 +66,106 @@ for (comparison in c('Se_Mi','Mod_Mi',  'Recovered_Deceased')){
   print(plotClusterMeanAndDiff(Res, cluster = Res$cluster))
   dev.off()
   
+  ## -----------
+  ## GO analysis
+  ## -----------
+  goRes <- GOEnrich(testobj = Res, type = 'variable')  
+  saveRDS(goRes, paste0(pdir, '/goRes.rds'))
+  
+  nn <- sapply(names(goRes), function(i){
+    tmp <- goRes[[i]]
+        tmp <- tmp[tmp[, 'FDR'] < 0.25, ]
+    write.csv(tmp, paste0(pdir, 'cluster', i, '_GO_FDR0.25.csv'))
+    tmp <- tmp[tmp[, 'FDR'] < 0.05, ]
+    write.csv(tmp, paste0(pdir, 'cluster', i, '_GO_FDR0.05.csv'))
+    print(str(tmp))
+    return(0)
+  })
+  
+  pdf(paste0(pdir, '/hm_GO_term5.pdf'), width = 6.8, height = 3.5)
+  print(plotGOEnrich(goRes))
+  dev.off()
+  
+  pdf(paste0(pdir, '/hm_GO_term10.pdf'), width = 6.8, height = 7)
+  print(plotGOEnrich(goRes, n = 10))
+  dev.off()
+  
+  pdf(paste0(pdir, '/hm_GO_term10_fdr0.25.pdf'), width = 6.8, height = 7)
+  print(plotGOEnrich(goRes, n = 10, fdr.cutoff = 0.25))
+  dev.off()
+  
+  pdf(paste0(pdir, '/hm_GO_term10_sortbyFC_fdr0.25.pdf'), width = 6.8, height = 7)
+  print(plotGOEnrich(goRes, n = 10, sortByFDR = F,fdr.cutoff = 0.25))
+  dev.off()
+  
   # --------------------------------------
   # compare original and fitted expression
   # --------------------------------------
-  # png(paste0(pdir, 'fitHm.png'),width = 4000,height = 2200,res = 300)
-  # plotFitHm(Res, type = 'variable')
+  png(paste0(pdir, 'DiffFitHm3.png'),width = 5000,height = 3000,res = 300)
+  plotDiffFitHm3(Res, cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = FALSE)
+  dev.off()
+  
+  # png(paste0(pdir, 'DiffFitHm3_sub.png'),width = 5000,height = 3000,res = 300)
+  # plotDiffFitHm3(Res,  cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = T)
+  # dev.off()
+
+  # png(paste0(pdir, 'DiffFitHm3_200.png'),width = 5000,height = 3000,res = 200)
+  # plotDiffFitHm3(Res, cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = FALSE)
   # dev.off()
   # 
-  # png(paste0(pdir, 'fitHm_rownames.png'),width = 12000,height = 10000,res = 300)
-  # print(plotFitHm(Res, type='variable', showRowName = T, cellWidthTotal = 1000, cellHeightTotal = length(Res$cluster) * 10))
+  png(paste0(pdir, 'DiffFitHm3_100.png'),width = 5000,height = 3000,res = 100)
+  plotDiffFitHm3(Res, cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = FALSE)
+  dev.off()
+  # 
+  # ########
+  # png(paste0(pdir, 'DiffFitHm3_clu_changepoint.png'),width = 5000,height = 3000,res = 100)
+  # plotDiffFitHm3(Res, cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = FALSE)
   # dev.off()
+  # 
+  # png(paste0(pdir, 'DiffFitHm3_clu_magnitute_cor.png'),width = 5000,height = 3000,res = 100)
+  # plotDiffFitHm3(Res, cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = FALSE)
+  # dev.off()
+  # 
+  # png(paste0(pdir, 'DiffFitHm3_clu_type_magnitute_changepoint.png'),width = 5000,height = 3000,res = 100)
+  # plotDiffFitHm3(Res, cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = FALSE)
+  # dev.off()
+  # 
   
   
-  png(paste0(pdir, 'DiffFitHm.png'),width = 4000,height = 2200,res = 200)
-  plotDiffFitHm(Res, type = 'variable', cellWidthTotal = 200, cellHeightTotal = 300)
+  groupdiff <- Res$covariateGroupDiff
+  mat1 <- groupdiff[names(clu)[clu == max(clu)-1], ]
+  mat2 <- groupdiff[names(clu)[clu == max(clu)], ]
+  dn1 <- dimnames(mat1)
+  dn2 <- dimnames(mat2)
+  v1 <- as.vector(mat1)
+  v2 <- as.vector(mat2)
+  mat1 <- matrix(v1/max(v1), nrow(mat1))
+  mat2 <- matrix(v2/max(v2), nrow(mat2))
+  dimnames(mat1) <- dn1
+  dimnames(mat2) <- dn2
+  
+  d1 <- apply(mat1, 1, max) - apply(mat1, 1, min)
+  d2 <- apply(mat2, 1, max) - apply(mat2, 1, min)
+  mat1 <- mat1[order(d1), ]  
+  mat2 <- mat2[order(d2), ]  
+  Res$covariateGroupDiff <- rbind(groupdiff[names(clu)[clu %in% seq(1, max(clu)-2)], ], mat1, mat2)
+   
+  png(paste0(pdir, 'DiffFitHm3_clu_type_changepoint_cor.png'),width = 5000,height = 3000,res = 100)
+  plotDiffFitHm3(Res, cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = F)
   dev.off()
   
-  png(paste0(pdir, 'DiffFitHm_rownames.png'),width = 12000,height = 10000,res = 300)
-  print(plotDiffFitHm(Res, type='variable', showRowName = T, cellWidthTotal = 1000, cellHeightTotal = length(Res$cluster) * 10))
-  dev.off()
+  # png(paste0(pdir, 'DiffFitHm_rownames3.png'),width = 12000,height = 10000,res = 300)
+  # print(plotDiffFitHm3(Res, type='variable', showRowName = T, cellWidthTotal = 1000, cellHeightTotal = length(Res$cluster) * 10))
+  # dev.off()
 
-  ## ----------
-  ## plot DDG
-  ## ----------
+  # ## ----------
+  # ## plot DDG
+  # ## ----------
   DDGType <- DDGType[diffgene]
-  id <- sort(sample(1:ncol(Res$populationFit[[1]]), ncol(Res$expr.ori)))
-  Res$populationFit[[1]] <- Res$populationFit[[1]][, id]
-  Res$populationFit[[2]] <- Res$populationFit[[2]][, id]
+  # id <- sort(sample(1:ncol(Res$populationFit[[1]]), ncol(Res$expr)))
+  # Res$populationFit[[1]] <- Res$populationFit[[1]][, id]
+  # Res$populationFit[[2]] <- Res$populationFit[[2]][, id]
+  # colnames(Res$population[[1]]) <- colnames(Res$population[[2]]) <- id
 
   for (i in unique(DDGType)){  ## debug -- ok!!
     print(i)
@@ -163,8 +190,6 @@ for (comparison in c('Se_Mi','Mod_Mi',  'Recovered_Deceased')){
     print(plotGenePopulation(testobj = Res, type = 'variable', gene = gene[1:min(length(gene), 100)], subSampleNumber=1000))
     dev.off()
   }
-  
 }
 
 
-  
