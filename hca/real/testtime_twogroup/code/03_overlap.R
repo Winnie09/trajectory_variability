c7739afc42d9a65df573c89bf16eead10d1afbc3
Wenpin Hou <wp.hou3@gmail.com>
2021-05-01 14:32:14 -0400

    03_overlap.R

--- hca/real/testtime_twogroup/code/03_overlap.R
+++ hca/real/testtime_twogroup/code/03_overlap.R
@@ -4,21 +4,24 @@ ddir <- 'hca/real/testtime_twogroup/result/'
 path = 'monocyte'
 
 overlap <- list()
-# method = 'EM_pm'
-# Res1 <- readRDS(paste0(ddir, method, '/', path, '/1256/testtime_res.rds'))
-# Res2 <- readRDS(paste0(ddir, method, '/', path, '/3478/testtime_res.rds'))
-# s1 = Res1$statistics
-# s2 = Res2$statistics
-# int <- intersect(rownames(s1), rownames(s2))
-# s1 = s1[int, ]
-# s1 = s1[order(s1[,1], -s1[,3]), ]
-# s2 = s2[int, ]
-# s2 = s2[order(s2[,1], -s2[,3]), ]
-# print(max(which(s1[,3]<0.05)))
-# o <- sapply((1:100)*100, function(i){
-#   length(intersect(rownames(s1)[1:i], rownames(s2)[1:i]))/i
-# })
-# mean(o)
+numdg2 <- numdg1 <- list()
+method = 'EM_pm'
+Res1 <- readRDS(paste0(ddir, method, '/', path, '/1256/testtime_res.rds'))
+Res2 <- readRDS(paste0(ddir, method, '/', path, '/3478/testtime_res.rds'))
+s1 = Res1$statistics
+s2 = Res2$statistics
+numdg1[[method]] <- sum(s1[,1] < 0.05)
+numdg2[[method]] <- sum(s2[,1] < 0.05)
+int <- intersect(rownames(s1), rownames(s2))
+s1 = s1[int, ]
+s1 = s1[order(s1[,1], -s1[,3]), ]
+s2 = s2[int, ]
+s2 = s2[order(s2[,1], -s2[,3]), ]
+o <- sapply((1:100)*100, function(i){
+  length(intersect(rownames(s1)[1:i], rownames(s2)[1:i]))/i
+})
+mean(o)
+overlap[[method]] <- mean(o)
 
 
 method = 'tradeSeq'
@@ -28,6 +31,9 @@ tmp <- Res1[['startVsEndTest']]
 s1 <- tmp[complete.cases(tmp), ]
 tmp <- Res2[['startVsEndTest']]
 s2 <- tmp[complete.cases(tmp), ]
+numdg1[[paste0(method, ':startVsEndTest')]] <- sum(s1[,3] < 0.05)
+numdg2[[paste0(method, ':startVsEndTest')]] <- sum(s2[,3] < 0.05)
+
 int <- intersect(rownames(s1), rownames(s2))
 s1 = s1[int, ]
 s1 = s1[order(s1[,3], -s1[,1]), ]
@@ -43,6 +49,9 @@ tmp <- Res1[['associationTest']]
 s1 <- tmp[complete.cases(tmp), ]
 tmp <- Res2[['associationTest']]
 s2 <- tmp[complete.cases(tmp), ]
+numdg1[[paste0(method, ':associationTest')]] <- sum(s1[,3] < 0.05)
+numdg2[[paste0(method, ':associationTest')]] <- sum(s2[,3] < 0.05)
+
 int <- intersect(rownames(s1), rownames(s2))
 s1 = s1[int, ]
 s1 = s1[order(s1[,3], -s1[,1]), ]
@@ -58,6 +67,9 @@ overlap[[paste0(method, ':associationTest')]] <- mean(o)
 method = 'tscan'
 s1 <- readRDS(paste0(ddir, method, '/', path, '/1256/testtime_res.rds'))
 s2 <- readRDS(paste0(ddir, method, '/', path, '/3478/testtime_res.rds'))
+numdg1[[method]] <- sum(s1[,3] < 0.05)
+numdg2[[method]] <- sum(s2[,3] < 0.05)
+
 int <- intersect(rownames(s1), rownames(s2))
 s1 = s1[int, ]
 s1 = s1[order(s1[,3], -s1[,2]), ]
@@ -73,6 +85,9 @@ overlap[[method]] <- mean(o)
 method = 'monocle2'
 s1 <- readRDS(paste0(ddir, method, '/', path, '/1256/testtime_res.rds'))
 s2 <- readRDS(paste0(ddir, method, '/', path, '/3478/testtime_res.rds'))
+numdg1[[method]] <- sum(s1[,3] < 0.05)
+numdg2[[method]] <- sum(s2[,3] < 0.05)
+
 int <- intersect(rownames(s1), rownames(s2))
 s1 = s1[int, ]
 s1 = s1[order(s1[,3], -s1[,2]), ]
@@ -83,5 +98,7 @@ o <- sapply((1:100)*100, function(i){
   length(intersect(rownames(s1)[1:i], rownames(s2)[1:i]))/i
 })
 overlap[[method]] <- mean(o)
-
 saveRDS(overlap, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testtime_twogroup/result/perf/overlap.rds')
+
+saveRDS(numdg1, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testtime_twogroup/result/perf/numdg1_1256.rds')
+saveRDS(numdg2, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testtime_twogroup/result/perf/numdg_3478.rds')
