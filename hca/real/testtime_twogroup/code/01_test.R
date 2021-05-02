1c39ba071afa251e612f6bb343242040a075d330
Wenpin Hou <wp.hou3@gmail.com>
2021-05-01 22:49:42 -0400

    01_test.R

--- hca/real/testtime_twogroup/code/01_test.R
+++ hca/real/testtime_twogroup/code/01_test.R
@@ -1,40 +1,96 @@
+# --------------
+# global setting
+# --------------
+method <- as.character(commandArgs(trailingOnly = T)[[1]])
+suppressMessages(library(parallel))
+suppressMessages(library(splines))
+suppressMessages(library(limma))
 library(here)
 setwd(here())
-for (path in c('monocyte', 'lymph', 'erythroid')){
-  ddir <- paste0('hca/real/build_from_tree_variability/result/', path, '/')
-  rdir <- paste0('hca/real/testtime_twogroup/data/', path, '/')
-  dir.create(rdir, recursive = TRUE, showWarnings = FALSE)
+ddir <- 'hca/real/testtime_twogroup/data/data/'
+rdir <- paste0('hca/real/testtime_twogroup/result/', method, '/monocyte/1256/')
+dir.create(rdir, recursive = TRUE, showWarnings = FALSE)
+source('./function/01_function.R')
+
+# ------------
+# prepare data
+# ------------
+expr = readRDS(paste0(ddir, 'expr1.rds'))
+cellanno = readRDS(paste0(ddir, 'cellanno1.rds'))
+rownames(cellanno) <- cellanno[,1]
+pseudotime = readRDS(paste0(ddir, 'pt1.rds'))
+design = readRDS(paste0(ddir, 'design1.rds'))
+expr <- expr[rowMeans(expr > 0.01) > 0.01, ]
+cnt <- readRDS('hca/data/proc/matrix/count.rds')
+cnt <- cnt[, colnames(expr)]
+cnt <- cnt[rowMeans(cnt > 0.01) > 0.01, ]
+# ----------------------------------
+# test  [one group along pseudotime]
+# ----------------------------------
+if (grepl('tradeSeq', method)){
+  suppressMessages(library(SingleCellExperiment))
+  suppressMessages(library(slingshot))
+  suppressMessages(library(tradeSeq))
+  expr <- cnt
+  cellanno = data.frame(cell=colnames(expr), sample = sub(':.*','', colnames(expr)), stringsAsFactors = FALSE)
+  ##
+  pdt <- data.frame(curve1 = pseudotime, curve2 = pseudotime)
+  rownames(pdt) <- names(pseudotime)
+  pdt = pdt[colnames(expr), ]
   
-  expr = readRDS(paste0(ddir, 'input_expr.rds'))
-  cellanno = readRDS(paste0(ddir, 'input_cellanno.rds'))
-  rownames(cellanno) <- cellanno[,1]
-  pseudotime = readRDS(paste0(ddir, 'input_pseudotime.rds'))
-  design = readRDS(paste0(ddir, 'input_design.rds'))
-  design = design[,1,drop=FALSE]
-  design <- design[sort(rownames(design)), , drop=F]
+  v <- (cellanno$sample %in% paste0('BM',seq(1,8)) + 0)
+  v <- ifelse(v==1, 0.99, 0.01)
+  cellWeights <- data.frame(curve1 = v, curve2 = 1-v)
+  rownames(cellWeights) <- colnames(expr)
   
-  selcell1 <- cellanno[cellanno[,2] %in% rownames(design)[c(1,2,5,6)], 1]
-  selcell2 <- setdiff(cellanno[,1], selcell1)
-  design1 <- design[c(1,2,5,6),,drop=F]
-  design2 <- design[setdiff(rownames(design), rownames(design1)),,drop=F]
-  cellanno1 <- cellanno[selcell1, ]
-  cellanno2 <- cellanno[selcell2, ]
-  pseudotime <- pseudotime[names(sort(pseudotime))]
-  pt1 <- seq(1, length(selcell1))
-  names(pt1) <- selcell1
-  pt2 <- seq(1, length(selcell2))
-  names(pt2) <- selcell2
+  set.seed(12345)
+  sce <- fitGAM(counts = round(expr), pseudotime = pdt, cellWeights = cellWeights,
+                nknots = 6, verbose = FALSE,parallel=F)
+  saveRDS(sce, paste0(rdir, 'sce.rds'))
   
-  saveRDS(selcell1, paste0(rdir, 'selcell1.rds'))
-  saveRDS(design1, paste0(rdir, 'design1.rds'))
-  saveRDS(cellanno1, paste0(rdir, 'cellanno1.rds'))
-  saveRDS(pt1, paste0(rdir, 'pseudotime1.rds'))
-  saveRDS(expr[, selcell1], paste0(rdir, 'expr1.rds'))
-  
-  saveRDS(selcell2, paste0(rdir, 'selcell2.rds'))
-  saveRDS(design2, paste0(rdir, 'design2.rds'))
-  saveRDS(cellanno2, paste0(rdir, 'cellanno2.rds'))
-  saveRDS(pt2, paste0(rdir, 'pseudotime2.rds'))
-  saveRDS(expr[,selcell2], paste0(rdir, 'expr2.rds'))
+  res <- list()
+  for (TestType in (c('startVsEndTest', 'associationTest'))){
+    print(TestType)
+    if (grepl('startVsEndTest', TestType)){
+      Res <- startVsEndTest(sce)
+    } else if (grepl('associationTest', TestType)){
+      Res <- associationTest(sce)
+    } 
+    resdf <- data.frame(waldStat = Res[,'waldStat'], P.Value = Res[,'pvalue'] ,adj.P.Val = p.adjust(Res$pvalue, method='fdr'))
+    row.names(resdf) <- row.names(Res)
+    resdf <- resdf[order(resdf[,3], -resdf[,1]), ]
+    res[[TestType]] <- resdf
+  }
 }
+
+if (method == 'EM_pm'){
+  system.time({
+    res <- testpt(expr=expr, cellanno=cellanno, pseudotime=pseudotime, design=design, ncores=24, test.type = 'Time', demean = FALSE, test.method = 'permutation', ncores.fit = 48)
+  })
+}
+
+if (method == 'EM_chisq'){
+  system.time({
+    res <- testpt(expr=expr, cellanno=cellanno, pseudotime=pseudotime, design=design, ncores=24, test.type = 'Time', demean = FALSE, test.method = 'chisq', ncores.fit = 48)
+  }) 
+}
+
+if (method == 'tscan'){
+  res <- TSCAN_time(expr=expr,pseudotime=pseudotime)
+}
+
+if (method == 'monocle2'){
+  res <- monocle2_time(expr=expr,pseudotime=pseudotime)
+}
+
+if (method == 'monocle3'){
+  library(spdep)
+  expr <- readRDS(paste0(ddir, 'saver/', dataType, '.rds'))
+  expr <- expr[, names(pseudotime)]
+  pca = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/data/simu/testtime/poolSampleSignal/null/hsc_mep_ery_integrated_pca.rds')
+  res <- monocle3_time(expr=expr, cell_coords = pca[,1:4])
+}
+saveRDS(res, paste0(rdir, 'testtime_res.rds'))
+
+
 
