66d6833992f39e968e20b8373cbb5665abc08a37
Wenpin Hou <wp.hou3@gmail.com>
2021-05-09 09:40:19 -0400

    20_monocyte_downstream_pipeline.R

--- hca/real/testvar/code/20_monocyte_downstream_pipeline.R
+++ hca/real/testvar/code/20_monocyte_downstream_pipeline.R
@@ -11,6 +11,7 @@ u1 = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/resource/chrX_genename.rds')
 u2 = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/resource/chrY_genename.rds')
 
 Res <- readRDS(paste0('hca/real/testvar/result/EM_pm/', path, '/gender/gender_res.rds'))
+Res$statistics <- Res$statistics[rownames(Res$statistics)!='RPS4Y1:ENSG00000129824',]
 print(names(Res))
 stat = Res$statistics
 diffgene <- rownames(stat[stat[, grep('^fdr.*overall$', colnames(stat))] < 0.05,])
@@ -20,7 +21,6 @@ stat <- stat[order(stat[,1], -stat[,3]), ]
 ## --------------
 ## population fit
 ## --------------
-
 Res$populationFit <- getPopulationFit(Res, gene = rownames(Res$expr), type = 'variable')
 
 ## -----------
@@ -92,36 +92,42 @@ dev.off()
 # plotDiffFitHm(Res, type = 'variable', cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = FALSE)
 # dev.off()
 
-png(paste0(pdir, 'DiffFitHm3.png'),width = 4500,height = 2200,res = 250)
-plotDiffFitHm3(Res,  cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = FALSE, break.0 = FALSE)
-dev.off()
+# png(paste0(pdir, 'DiffFitHm3.png'),width = 4500,height = 2200,res = 250)
+# plotDiffFitHm3(Res,  cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = FALSE, break.0 = FALSE)
+# dev.off()
 
 png(paste0(pdir, 'DiffFitHm3_rownames.png'),width = 7500,height = 3500,res = 300)
-plotDiffFitHm3(Res, showRowName = T, cellWidthTotal = 300, cellHeightTotal = length(Res$cluster) * 10, sep = ':.*', , subsampleCell = FALSE, break.0 = FALSE)
+plotDiffFitHm3(Res, showRowName = T, cellWidthTotal = 300, cellHeightTotal = length(Res$cluster) * 10, sep = ':.*', subsampleCell = FALSE, break.0 = FALSE)
 dev.off()
 
+
+png(paste0(pdir, 'DiffFitHm4_rownames.png'),width = 7500,height = 3500,res = 300)
+plotDiffFitHm4(Res, showRowName = T, cellWidthTotal = 300, cellHeightTotal = length(Res$cluster) * 10, sep = ':.*' , subsampleCell = FALSE, break.0 = FALSE)
+dev.off()
+
+
 groupdiff <- Res$covariateGroupDiff
-  mat1 <- groupdiff[names(clu)[clu == max(clu)-1], ]
-  mat2 <- groupdiff[names(clu)[clu == max(clu)], ]
-  dn1 <- dimnames(mat1)
-  dn2 <- dimnames(mat2)
-  v1 <- as.vector(mat1)
-  v2 <- as.vector(mat2)
-  mat1 <- matrix(v1/max(v1), nrow(mat1))
-  mat2 <- matrix(v2/max(v2), nrow(mat2))
-  dimnames(mat1) <- dn1
-  dimnames(mat2) <- dn2
-  
-  d1 <- apply(mat1, 1, max) - apply(mat1, 1, min)
-  d2 <- apply(mat2, 1, max) - apply(mat2, 1, min)
-  mat1 <- mat1[order(d1), ]  
-  mat2 <- mat2[order(d2), ]  
-  Res$covariateGroupDiff <- rbind(groupdiff[names(clu)[clu %in% seq(1, max(clu)-2)], ], mat1, mat2)
-   
-  png(paste0(pdir, 'DiffFitHm3_clu_type_changepoint_cor.png'),width = 5000,height = 3000,res = 100)
-  plotDiffFitHm3(Res, cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = F)
-  dev.off()
-  
+mat1 <- groupdiff[names(clu)[clu == max(clu)-1], ]
+mat2 <- groupdiff[names(clu)[clu == max(clu)], ]
+dn1 <- dimnames(mat1)
+dn2 <- dimnames(mat2)
+v1 <- as.vector(mat1)
+v2 <- as.vector(mat2)
+mat1 <- matrix(v1/max(v1), nrow(mat1))
+mat2 <- matrix(v2/max(v2), nrow(mat2))
+dimnames(mat1) <- dn1
+dimnames(mat2) <- dn2
+
+d1 <- apply(mat1, 1, max) - apply(mat1, 1, min)
+d2 <- apply(mat2, 1, max) - apply(mat2, 1, min)
+mat1 <- mat1[order(d1), ]  
+mat2 <- mat2[order(d2), ]  
+Res$covariateGroupDiff <- rbind(groupdiff[names(clu)[clu %in% seq(1, max(clu)-2)], ], mat1, mat2)
+
+png(paste0(pdir, 'DiffFitHm3_clu_type_changepoint_cor.png'),width = 5000,height = 3000,res = 100)
+plotDiffFitHm3(Res, cellWidthTotal = 200, cellHeightTotal = 300, subsampleCell = F)
+dev.off()
+
 # ## ----------
 # ## plot DDG
 # ## ----------
@@ -155,8 +161,19 @@ groupdiff <- Res$covariateGroupDiff
 #   dev.off()
 # }
 
+expr <- Res$expr[c(row.names(Res$expr)[1:100],"RPS4Y1:ENSG00000129824","CD3D:ENSG00000167286","CD3G:ENSG00000160654"),]
+cellanno <- Res$cellanno  
+pseudotime <- Res$pseudotime
+design <- Res$design
+permuiter=100;EMmaxiter=100;EMitercutoff=0.1;verbose=F;ncores=detectCores();test.type='Time';fit.resolution = 1000;return.all.data = TRUE;demean = FALSE;overall.only = F;test.method = 'permutation';ncores.fit = 1;fix.all.zero = TRUE;cutoff = 1e-5
 
+rr <- testpt(Res$expr[c(row.names(Res$expr)[1:100],"RPS4Y1:ENSG00000129824","CD3D:ENSG00000167286","CD3G:ENSG00000160654"),], Res$cellanno, Res$pseudotime, Res$design,test.type='Variable',ncores=10)
 
+png(paste0(pdir, 'RPS4Y1.png'),width = 1000,height = 1000,res = 100)
+plotGene(Res, gene = "RPS4Y1:ENSG00000129824", plot.point = T)
+dev.off()
 
-
+png(paste0(pdir, 'RPS4Y1_new.png'),width = 1000,height = 1000,res = 100)
+plotGene(rr, gene = "RPS4Y1:ENSG00000129824", plot.point = T)
+dev.off()
 
