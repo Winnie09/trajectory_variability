130521b71052475387b3ba07172c7463cb62a7df
Wenpin Hou <wp.hou3@gmail.com>
2021-05-10 09:30:59 -0400

    04.2_trendFDR_and_meanFDR_perf.R

--- hca/simu/testvar/addMultiSignalUsingExpr/code/04.2_trendFDR_and_meanFDR_perf.R
+++ hca/simu/testvar/addMultiSignalUsingExpr/code/04.2_trendFDR_and_meanFDR_perf.R
@@ -1,125 +1,106 @@
+rm(list=ls())
+#setwd('/Users/wenpinhou/Dropbox/trajectory_variability')
 library(here)
 setwd(here())
-ddir <- 'hca/real/testtime_twogroup/result/'
-path = 'monocyte'
+ddir <- 'hca/simu/testvar/addMultiSignalUsingExpr/result/'
+rdir <- 'hca/simu/testvar/addMultiSignalUsingExpr/result/perf/'
+pdir <- 'hca/simu/testvar/addMultiSignalUsingExpr/plot/perf/'
+selgene1 <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/data/selgene/selgene1.rds') # trend diff only
+selgene2 <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/data/selgene/selgene2.rds') # mean diff only
+selgene3 <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/data/selgene/selgene3.rds') # trend and mean
+selgene = selgene1
+rmgene = c(selgene2, selgene3)
+str(selgene)
+str(rmgene)
+source('function/01_function.R')
+library(gridExtra)
 
-omat <- overlap <- list()
-numdg2 <- numdg1 <- list()
-method = 'EM_pm'
-Res1 <- readRDS(paste0(ddir, method, '/', path, '/1256/testtime_res.rds'))
-Res2 <- readRDS(paste0(ddir, method, '/', path, '/3478/testtime_res.rds'))
-s1 = Res1$statistics
-s2 = Res2$statistics
-numdg1[[method]] <- sum(s1[,1] < 0.05)
-numdg2[[method]] <- sum(s2[,1] < 0.05)
-int <- intersect(rownames(s1), rownames(s2))
-s1 = s1[int, ]
-s1 = s1[order(s1[,1], -s1[,3]), ]
-s2 = s2[int, ]
-s2 = s2[order(s2[,1], -s2[,3]), ]
-o <- sapply((1:100)*100, function(i){
-  length(intersect(rownames(s1)[1:i], rownames(s2)[1:i]))/i
+perflist <- list()
+m = 'EM_pm'
+af = list.files(paste0(ddir, m, '/'))
+df1.nc <- sapply(af, function(f){
+  print(f)
+  r = readRDS(paste0(ddir, m, '/', f))
+  res = data.frame(adj.P.Val = r$statistics[,'fdr.trendDiff'], zscore = r$statistics[,'z.trendDiff'], stringsAsFactors = F)
+  res = res[complete.cases(res),]
+  res = res[order(res[,1], -res[,2]), ]
+  c(sub('.rds','',f), 'trendDiff', AreaUnderSensFdr(SensFdr(c(selgene1,selgene3), res)))
 })
-mean(o)
-omat[[method]] <- o
-overlap[[method]] <- mean(o)
+perflist[['trendDiff']] = t(df1.nc)
 
-method = 'tradeSeq'
-Res1 <- readRDS(paste0(ddir, method, '/', path, '/1256/testtime_res.rds'))
-Res2 <- readRDS(paste0(ddir, method, '/', path, '/3478/testtime_res.rds'))
-tmp <- Res1[['startVsEndTest']]
-s1 <- tmp[complete.cases(tmp), ]
-tmp <- Res2[['startVsEndTest']]
-s2 <- tmp[complete.cases(tmp), ]
-numdg1[[paste0(method, ':startVsEndTest')]] <- sum(s1[,3] < 0.05)
-numdg2[[paste0(method, ':startVsEndTest')]] <- sum(s2[,3] < 0.05)
-
-int <- intersect(rownames(s1), rownames(s2))
-s1 = s1[int, ]
-s1 = s1[order(s1[,3], -s1[,1]), ]
-s2 = s2[int, ]
-s2 = s2[order(s2[,3], -s2[,1]), ]
-print(max(which(s1[,3]<0.05)))
-o <- sapply((1:100)*100, function(i){
-  length(intersect(rownames(s1)[1:i], rownames(s2)[1:i]))/i
+df1.nc <- sapply(af, function(f){
+  print(f)
+  r = readRDS(paste0(ddir, m, '/', f))
+  res = data.frame(adj.P.Val = r$statistics[,'fdr.meanDiff'], zscore = r$statistics[,'z.meanDiff'], stringsAsFactors = F)
+  res = res[complete.cases(res),]
+  res = res[order(res[,1], -res[,2]), ]
+  c(sub('.rds','',f), 'meanDiff', AreaUnderSensFdr(SensFdr(c(selgene2,selgene3), res)))
 })
-omat[[paste0(method, ':startVsEndTest')]] <- o
-overlap[[paste0(method, ':startVsEndTest')]] <- mean(o)
+perflist[['meanDiff']] = t(df1.nc)
 
-tmp <- Res1[['associationTest']]
-s1 <- tmp[complete.cases(tmp), ]
-tmp <- Res2[['associationTest']]
-s2 <- tmp[complete.cases(tmp), ]
-numdg1[[paste0(method, ':associationTest')]] <- sum(s1[,3] < 0.05)
-numdg2[[paste0(method, ':associationTest')]] <- sum(s2[,3] < 0.05)
+perf <- do.call(rbind,perflist)
+perf <- data.frame('SignalStrength'=as.numeric(perf[,1]),'Method'=perf[,2],'Fdr.Diff'=as.numeric(perf[,3]),'AUC'=as.numeric(perf[,4]))
+saveRDS(perf, paste0(rdir,'perf_trenddiff_meandiff.rds'))
 
-int <- intersect(rownames(s1), rownames(s2))
-s1 = s1[int, ]
-s1 = s1[order(s1[,3], -s1[,1]), ]
-s2 = s2[int, ]
-s2 = s2[order(s2[,3], -s2[,1]), ]
-print(max(which(s1[,3]<0.05)))
-o <- sapply((1:100)*100, function(i){
-  length(intersect(rownames(s1)[1:i], rownames(s2)[1:i]))/i
-})
-omat[[paste0(method, ':associationTest')]] <- o
-overlap[[paste0(method, ':associationTest')]] <- mean(o)
 
-##
-method = 'tscan'
-s1 <- readRDS(paste0(ddir, method, '/', path, '/1256/testtime_res.rds'))
-s2 <- readRDS(paste0(ddir, method, '/', path, '/3478/testtime_res.rds'))
-numdg1[[method]] <- sum(s1[,3] < 0.05)
-numdg2[[method]] <- sum(s2[,3] < 0.05)
+pdonly <- perf
+pdonly$comparison <- sub('meanDiff','FDR.meanSig',sub('trendDiff','FDR.trendSig',pdonly$Method))
+pdonly$Method <- 'Lamian'
 
-int <- intersect(rownames(s1), rownames(s2))
-s1 = s1[int, ]
-s1 = s1[order(s1[,3], -s1[,2]), ]
-s2 = s2[int, ]
-s2 = s2[order(s2[,3], -s2[,2]), ]
-print(max(which(s1[,3]<0.05)))
-o <- sapply((1:100)*100, function(i){
-  length(intersect(rownames(s1)[1:i], rownames(s2)[1:i]))/i
-})
-omat[[method]] <- o
-overlap[[method]] <- mean(o)
+pd <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/result/perf/perf.rds')
+pd[,2] <- as.character(pd[,2])
+pd0 <- pd[pd[,2]!='EM_chisq', ]
 
-## monocle2
-method = 'monocle2'
-s1 <- readRDS(paste0(ddir, method, '/', path, '/1256/testtime_res.rds'))
-s2 <- readRDS(paste0(ddir, method, '/', path, '/3478/testtime_res.rds'))
-numdg1[[method]] <- sum(s1[,3] < 0.05)
-numdg2[[method]] <- sum(s2[,3] < 0.05)
+pd0$comparison <- 'overall'
+pd <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/result/perf/perf_meanOnly.rds')
+pd[,2] <- as.character(pd[,2])
+pd1 <- pd[pd[,2]!='EM_chisq', ]
 
-int <- intersect(rownames(s1), rownames(s2))
-s1 = s1[int, ]
-s1 = s1[order(s1[,3], -s1[,2]), ]
-s2 = s2[int, ]
-s2 = s2[order(s2[,3], -s2[,2]), ]
-print(max(which(s1[,3]<0.05)))
-o <- sapply((1:100)*100, function(i){
-  length(intersect(rownames(s1)[1:i], rownames(s2)[1:i]))/i
-})
-omat[[method]] <- o
-overlap[[method]] <- mean(o)
-saveRDS(overlap, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testtime_twogroup/result/perf/overlap.rds')
-saveRDS(numdg1, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testtime_twogroup/result/perf/numdg1_1256.rds')
-saveRDS(numdg2, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testtime_twogroup/result/perf/numdg_3478.rds')
+pd1$comparison = 'mean only'
+pd <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/result/perf/perf_trendOnly.rds')
+pd[,2] <- as.character(pd[,2])
+pd2 <- pd[pd[,2]!='EM_chisq', ]
 
-omat <- do.call(cbind, omat)
-saveRDS(omat, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testtime_twogroup/result/perf/overlap_all.rds')
+pd2$comparison = 'trend only'
+pd <- readRDS('hca/simu/testvar/addMultiSignalUsingExpr/result/perf/perf_trendMean.rds')
+pd[,2] <- as.character(pd[,2])
+pd3 <- pd[pd[,2]!='EM_chisq', ]
 
-# omat <-readRDS('/Users/wenpinhou/Dropbox/trajectory_variability/hca/real/testtime_twogroup/result/perf/overlap_all.rds')
-library(reshape2)
-pd <- melt(omat)
+pd3$comparison = 'trend & mean'
+pd <- rbind(pd0, pd1,pd2,pd3)
+pd <- pd[pd[,2]!='TSCAN', ]
+pd$comparison <- factor(pd$comparison, levels = c('overall', 'trend only', 'trend & mean','mean only'))
+pd[pd[,2] == 'EM_pm', 2] <- 'Lamian' ## method name
+
+pd <- rbind(pd,pdonly)
+pd <- pd[complete.cases(pd), ]
 library(ggplot2)
-library(RColorBrewer)
-ggplot() + geom_freqpoly(data = pd, aes(value,color = Var2), fill = NA, binwidth = 0.01) + scale_color_brewer(palette = 'Set1') + theme_classic()
-
-pdf('/Users/wenpinhou/Dropbox/trajectory_variability/hca/real/testtime_twogroup/plot/overlap_proportion_all.pdf', width = 6, height = 3.5)
-ggplot(data = pd, aes(x=Var1, y = value, color = Var2)) + geom_line(size = 0.1) + 
-  geom_point(size = 0.1) +
-  scale_color_brewer(palette = 'Set1') + theme_classic() + 
-  xlab('top n*10 genes') + ylab('overlap proportion')
+pdf(paste0(pdir, 'compare_fdr_diff_auc_trenddiff_meandiff.pdf'),width=12.5,height=2.9)
+p1 <- ggplot(pd, aes(x = SignalStrength, y = Fdr.Diff, color=Method)) + 
+  geom_point(size=2)  + 
+  geom_line(size=1) + 
+  theme_bw() + 
+  xlim(c(0,4))+
+  ylab('FDR.difference') +
+  scale_color_brewer(palette = 'Set1')  +
+  facet_wrap(~comparison, nrow =1) +
+  theme(legend.position = 'bottom',legend.title = element_blank()) + 
+  ylim(c(-0.04,0.195))+
+  guides(color=guide_legend(nrow=2,byrow=TRUE))
+p2 <- ggplot(pd, aes(x = SignalStrength, y = AUC, color=Method)) + 
+  geom_point(size=2)  + 
+  geom_line(size=1) + 
+  theme_bw() + 
+    xlim(c(0,4))+
+    ylab('AUC (sensitivity-realFDR)') +
+  scale_color_brewer(palette = 'Set1')  +
+  facet_wrap(~comparison, nrow =1)+
+    theme(legend.position = 'bottom',legend.title = element_blank()) + 
+  ylim(c(0,1))+
+    guides(color=guide_legend(nrow=2,byrow=TRUE))
+grid.arrange(p1,p2,nrow=1)
 dev.off()
+
+
+
 
