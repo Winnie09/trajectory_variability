61dbb609cc17f1f0d6cb24f620c0e4e7e856c465
Wenpin Hou <wp.hou3@gmail.com>
2021-06-01 10:13:37 -0400

    01_preparedata.R:add more proportion changes

--- hca/simu/testvar/cellprop/code/01_preparedata.R
+++ hca/simu/testvar/cellprop/code/01_preparedata.R
@@ -2,7 +2,6 @@ library(here)
 setwd(here())
 source('function/01_function.R')
 for (path in c('erythroid', 'monocyte', 'lymph')){
-    
   ddir <- paste0('hca/real/build_from_tree_variability/result/', path, '/')
   rdir <- paste0('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/simu/testvar/cellprop/data/',path,'/')
   dir.create(rdir, recursive = T)
@@ -15,7 +14,7 @@ for (path in c('erythroid', 'monocyte', 'lymph')){
   samp <- sub(':.*','',names(pt))
   names(samp) <- names(pt)
   set.seed(12345)
-  for (prop in c(0.01,0.05,0.1,0.25,0.5)) {
+  for (prop in c(0.01,0.05, 0.25,seq(0.1, 0.9, 0.1))) {
     print(paste0(path, '_', prop))
     spt <- pt
     exccell <- intersect(names(samp)[which(samp %in% rownames(design)[design[,2]==1])],names(pt)[pt < median(pt)])
@@ -23,4 +22,9 @@ for (path in c('erythroid', 'monocyte', 'lymph')){
     saveRDS(spt,file=paste0(rdir, prop,'.rds'))
   }
 }  
+
+
+# ggplot(data.frame(pt=pt,samp=sub('_.*','',names(pt))),aes(pt,col=samp)) + geom_density() + facet_wrap(~samp)
+
+# ggplot(data.frame(pt=spt,samp=sub('_.*','',names(spt))),aes(pt,col=samp)) + geom_density() + facet_wrap(~samp)
 
