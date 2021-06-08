a3a4e1a430ee6257d92e647b585853b2a6b866ff
Wenpin Hou <wp.hou3@gmail.com>
2021-06-08 09:15:59 -0400

    getDDGType.R

---
+++ function/getDDGType.R
@@ -0,0 +1,25 @@
+getDDGType <- function(testobj, cutoff = 0.05){
+  res <- testobj$statistics
+  if (toupper(testobj$test.type) == 'VARIABLE'){
+    diffType <- sapply(rownames(res), function(g){
+      if (res[g, grep('^fdr.*overall$', colnames(res))] < cutoff){
+        if (res[g, grep('^fdr.*meanDiff$', colnames(res))] < cutoff & res[g, grep('^fdr.*trendDiff$', colnames(res))] > cutoff){
+          'meanSig'
+        } else if (res[g, grep('^fdr.*meanDiff$', colnames(res))] > cutoff & res[g, grep('^fdr.*trendDiff$', colnames(res))] < cutoff){
+          'trendSig'
+        } else if (res[g, grep('^fdr.*meanDiff$', colnames(res))] < cutoff & res[g, grep('^fdr.*trendDiff$', colnames(res))] < cutoff){
+          'bothSig'
+        } else {
+          'other'
+        }
+      } else {
+        'nonDDG'
+      }
+    })
+  } else if (toupper(testobj$test.type) == 'TIME'){
+    print('ConstantTimeTest does not lead to DDGType!')
+  }
+  return(diffType)
+}
+
+
