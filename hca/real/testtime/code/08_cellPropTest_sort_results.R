e9662185aef27bdc33f52d84e5daac96659a571e
Wenpin Hou <wp.hou3@gmail.com>
2021-06-18 07:09:58 -0400

    add changes

--- hca/real/testtime/code/08_cellPropTest_sort_results.R
+++ hca/real/testtime/code/08_cellPropTest_sort_results.R
@@ -4,11 +4,16 @@ setwd(here())
 source('function/01_function.R')
 res <- list()
 for (path in c('erythroid', 'lymph', 'monocyte')){
+  print(path)
     pdir <- paste0('hca/real/testtime/plot/EM_pm/', path, '/')
-    Res <- readRDS(paste0('hca/real/testtime/result/EM_pm/', path, '/cell_proportion_test.rds'))
+    Res <- readRDS(paste0('hca/real/testtime/result/EM_pm/', path, '/gender/cell_proportion_test.rds'))
     
-    pdf(paste0(pdir, 'cellPropTest.pdf'), width = 3, height = 2)
-    plotGene(testobj = Res, gene='prop', cellProp = TRUE,  variable.text = NULL, free.scale = TRUE, facet.sample = FALSE, plot.point = F, line.alpha = 1, line.size = 1, point.alpha=1, point.size=0.5, continuous = T, sep = NA, palette = 'Dark2', ncol = NULL,  axis.text.blank = T, y.lab = 'Cell density')
+    pdf(paste0(pdir, 'cellPropTest_gender.pdf'), width = 2.6, height = 1.9)
+    plotGene(testobj = Res, gene='prop', cellProp = TRUE,  variable.text = NULL, free.scale = TRUE, facet.sample = FALSE, plot.point = F, line.alpha = 1, line.size = 0.4, point.alpha=0.8, point.size=0.1, continuous = T, sep = NA, palette = 'Dark2', ncol = NULL,  axis.text.blank = T, y.lab = 'Cell density')
+    dev.off()
+    
+    pdf(paste0(pdir, 'cellPropTest_gender_point.pdf'), width = 2.6, height = 1.9)
+    plotGene(testobj = Res, gene='prop', cellProp = TRUE,  variable = 'gender', variable.text = NULL, free.scale = TRUE, facet.sample = FALSE, plot.point = T, line.alpha = 1, line.size = 0.4, point.alpha=0.8, point.size=0.5, continuous = F, sep = NA, palette = 'Dark2', use.palette=T, ncol = NULL,  axis.text.blank = F, y.lab = 'Cell density')
     dev.off()
     
     pt = Res$pseudotime
@@ -18,7 +23,7 @@ for (path in c('erythroid', 'lymph', 'monocyte')){
     ld <- data.frame(den = pred[1,], pt = pt[colnames(pred)], sample = cellanno[match(colnames(pred),cellanno[,1]),2])
     pd <- data.frame(den = Res$expr[1,], pt = pt[colnames(Res$expr)], sample = cellanno[match(colnames(expr), cellanno[,1]), 2])
     library(ggplot2)
-    pdf(paste0(pdir, 'cellPropTest_fitting.pdf'), width = 3, height = 6)
+    pdf(paste0(pdir, 'cellPropTest_fitting_gender.pdf'), width = 3, height = 6)
     print(ggplot() + 
       geom_line(data = ld, aes(x = pt, y = den), size = 0.5)  +
       geom_point(data = pd, aes(x =pt, y = den), size = 0.2)+
@@ -30,4 +35,34 @@ for (path in c('erythroid', 'lymph', 'monocyte')){
 res <- do.call(rbind, res)
 dir.create('hca/real/testtime/result/EM_pm/perf/', recursive = T)
 write.csv(res, 'hca/real/testtime/result/EM_pm/perf/cellprop_res.csv')
+
+###############################
+for (path in c('erythroid', 'lymph', 'monocyte')){
+  print(path)
+    pdir <- paste0('hca/real/testtime/plot/EM_pm/', path, '/')
+    Res <- readRDS(paste0('hca/real/testtime/result/EM_pm/', path, '/age/cell_proportion_test.rds'))
+    
+    pdf(paste0(pdir, 'cellPropTest_age.pdf'), width = 2.8, height = 1.9)
+    plotGene(testobj = Res, gene='prop', cellProp = TRUE,  variable.text = NULL, free.scale = TRUE, facet.sample = FALSE, plot.point = T, line.alpha = 1, line.size = 0.4, point.alpha=0.8, point.size=0.5, continuous = T, sep = NA, palette = 'Dark2', ncol = NULL,  axis.text.blank = T, y.lab = 'Cell density')
+    dev.off()
+    
+    pdf(paste0(pdir, 'cellPropTest_age_point.pdf'), width = 2.3, height = 1.9)
+    plotGene(testobj = Res, gene='prop', cellProp = TRUE,  variable = 'age', variable.text = NULL, free.scale = TRUE, facet.sample = FALSE, plot.point = T, line.alpha = 1, line.size = 0.4, point.alpha=0.8, point.size=0.5, continuous = T, sep = NA, palette = 'Dark2', use.palette=F, ncol = NULL,  axis.text.blank = F, y.lab = 'Cell density')
+    dev.off()
+    
+    pt = Res$pseudotime
+    expr = Res$expr 
+    cellanno = Res$cellanno
+    pred <- predict_fitting(Res, gene = 'prop', test.type = Res$test.type)
+    ld <- data.frame(den = pred[1,], pt = pt[colnames(pred)], sample = cellanno[match(colnames(pred),cellanno[,1]),2])
+    pd <- data.frame(den = Res$expr[1,], pt = pt[colnames(Res$expr)], sample = cellanno[match(colnames(expr), cellanno[,1]), 2])
+    library(ggplot2)
+    pdf(paste0(pdir, 'cellPropTest_fitting_age.pdf'), width = 3, height = 6)
+    print(ggplot() + 
+      geom_line(data = ld, aes(x = pt, y = den), size = 0.5)  +
+      geom_point(data = pd, aes(x =pt, y = den), size = 0.2)+
+      facet_wrap(~sample, nrow = 4) +
+      theme_minimal() + xlab('Pseudotime bin') + ylab('Density'))
+    dev.off()
+}
 
