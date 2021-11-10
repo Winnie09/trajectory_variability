cc610da18d4548c20d012308d9b4a3ffbd70e0ff
Wenpin Hou <wp.hou3@gmail.com>
2021-11-10 06:08:20 -0500

    20211109 01_get_sacct_out.R

--- efficiency/code/01_get_sacct_out.R
+++ efficiency/code/01_get_sacct_out.R
@@ -3,7 +3,7 @@ do
 sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> Lamian_hca_simu_cellprop.txt
 done
 
-###
+### hca-bm simulation
 for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/simu/testvar/addMultiSignalUsingExpr/code/ | grep run01.EM_pm| grep .sh.e | sed 's/.*.sh.e//g'`
 do
 sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> Lamian_hca_simu_xde.txt
@@ -15,7 +15,38 @@ do
 sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> tradeSeq_hca_simu_xde.txt
 done
 
-###
+
+## 
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/simu/testvar/addMultiSignalUsingExpr/code/ | grep phenograph100.sh| grep .sh.e | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> phenograph100_hca_simu_xde.txt
+done
+
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/simu/testvar/addMultiSignalUsingExpr/code/ | grep phenograph500.sh| grep .sh.e | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> phenograph500_hca_simu_xde.txt
+done
+
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/simu/testvar/addMultiSignalUsingExpr/code/ | grep run01.chisq.sh| grep .sh.e | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> Lamian.chisq_hca_simu_xde.txt
+done
+
+
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/simu/testvar/addMultiSignalUsingExpr/code/ | grep condiments.sh| grep .sh.e | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> condiments_hca_simu_xde.txt
+done
+
+
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/simu/testvar/addMultiSignalUsingExpr/code/ | grep monocle2_trajtest.sh| grep .sh.e | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> monocle2.trajtest.sh_hca_simu_xde.txt
+done
+
+
+
+### real data: hca
 for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testvar/code/ | grep .EM.pm.sh.e | sed 's/.*.sh.e//g'`
 do
 sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> Lamian_hca_xde.txt
@@ -33,7 +64,24 @@ do
 sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> Lamian_covid_Mod_Mi.txt
 done
 
+## new methods
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/covid/Su_2020_Cell/testvar/useSaverImp/code/ | grep "run35.2.sh | run34.2.sh | run36.2.sh" | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> phenopath100_covid_Mod_Mi.txt
+done
 
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/covid/Su_2020_Cell/testvar/useSaverImp/code/ | grep " run34.2.sh" | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> phenopath100_covid_Mod_Mi.txt
+done
+
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/covid/Su_2020_Cell/testvar/useSaverImp/code/ | grep "run36.2.sh" | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> phenopath100_covid_Mod_Mi.txt
+done
+
+
+### real data: covid
 for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/covid/Su_2020_Cell/testvar/useSaverImp/compcode/tradeSeq/ | grep run.sh.e | sed 's/.*.sh.e//g'`
 do
 sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> tradeSeq_covid_Mod_Mi.txt
@@ -49,6 +97,7 @@ for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/covid/
 do
 sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> Lamian_covid_cellprop.txt
 done
+
 
 
 
