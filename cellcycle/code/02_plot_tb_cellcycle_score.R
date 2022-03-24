bfb170e9f24f8e8345cba7f87aaa0c1e959799e8
Wenpin Hou <wp.hou3@gmail.com>
2022-03-24 08:26:23 -0400

    02_plot_tb_cellcycle_score.R

---
+++ cellcycle/code/02_plot_tb_cellcycle_score.R
@@ -0,0 +1,19 @@
+pd = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/cellcycle/res/tb_cellcycle_score.rds')
+library(ggplot2)
+source('/home-4/whou10@jhu.edu/scratch/Wenpin/resource/ggplot_theme.R')
+theme_set(.new_theme)
+
+colv = c('orange', 'royalblue')
+names(colv) = c('female', 'male') 
+
+pdf('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/cellcycle/plot/tb_cellcycle_score_s.pdf', width = 2, height = 1.5)
+ggplot(data = pd) + geom_line(aes(x = pt.sample, y = cc.s.sample, color = sex, group = sample), size = 0.1, alpha = 0.8) + xlab('pseudotime') + ylab('cellcycle score (s genes)') + scale_color_manual(values = colv)
+dev.off()
+
+
+pdf('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/cellcycle/plot/tb_cellcycle_score_g2m.pdf', width = 2, height = 1.5)
+ggplot(data = pd) + geom_line(aes(x = pt.sample, y = cc.g2m.sample, color = sex, group = sample), size = 0.1, alpha = 0.8) + xlab('pseudotime') + ylab('cellcycle score (G2-M genes)') + scale_color_manual(values = colv)
+dev.off()
+
+
+
