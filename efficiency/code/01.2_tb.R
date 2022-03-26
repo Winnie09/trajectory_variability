421d8323d52a98d4fe831869e9b5d773cb81b2a3
Wenpin Hou <wp.hou3@gmail.com>
2022-03-26 11:04:55 -0400

    01.2_tb.R: output each method seperately

--- efficiency/code/01.2_tb.R
+++ efficiency/code/01.2_tb.R
@@ -1,7 +1,33 @@
-
 ## tb xde
-for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/code/sex/run/ | grep .sh.e | sed 's/.*.sh.e//g'`
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/code/sex/run/ | grep lamian.pm.sh.e | sed 's/.*.sh.e//g'`
 do
-sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> tb_xde.txt
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> Lamian.pm_tb_xde.txt
 done
 
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/code/sex/run/ | grep condiments.sh.e | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> condiments_tb_xde.txt
+done
+
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/code/sex/run/ | grep lamian.chisq.sh.e | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> Lamian.chisq_tb_xde.txt
+done
+
+
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/code/sex/run/ | grep monocle2_trajtest_corr.sh.e | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> monocle2TrajtestCorr_tb_xde.txt
+done
+
+
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/code/sex/run/ | grep monocle2_trajtest.sh.e | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> monocle2Trajtest_tb_xde.txt
+done
+
+
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/code/sex/run/ | grep tradeSeq.sh.e | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> tradeSeq_tb_xde.txt
+done
