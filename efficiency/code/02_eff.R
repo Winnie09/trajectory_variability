fef080259da36f6a3856f16d052c86fe848bf458
Wenpin Hou <wp.hou3@gmail.com>
2022-03-26 11:24:58 -0400

    02_eff.R: move plot code out

--- efficiency/code/02_eff.R
+++ efficiency/code/02_eff.R
@@ -15,12 +15,13 @@ af = list.files(getwd(), pattern = '.txt')
 eff <- lapply(af,function(f){
   print(f)
   d=readLines(f)
+  
   a = sapply(d[grep('whou10',d)+1],function(i) {
     tmp <- strsplit(i,' ')[[1]]
     tmp <- tmp[nchar(tmp) > 0]
     if ('COMPLETED' %in% tmp) {
       tmp[(length(tmp)-1):length(tmp)]  
-    } else if ('FAILED' %in% tmp | 'RUNNING' %in% tmp) {
+    } else if ('FAILED' %in% tmp | 'RUNNING' %in% tmp | 'CANCELLED' %in% tmp) {
       c(NA,NA)
     }
   },USE.NAMES = F, simplify = F)
@@ -30,9 +31,9 @@ eff <- lapply(af,function(f){
   }
   a = a[,order(as.numeric(sub('K','',a[2,]))), drop = F]
   a[1,] <- sapply(a[1,],timefunc)
-  rownames(a) = c('time', 'memory')
+  rownames(a) = c('time(h)', 'memory(GB)')
   a.mt = melt(a)
-  df = data.frame(type=as.character(a.mt[,1]), id=as.character(a.mt[,2]), value=as.character(a.mt[,3]), method = sub('_.*', '', f), data = sub(paste0(strsplit(f, '_')[[1]][1],'_'), '', sub('.txt','',f)), stringsAsFactors = F)
+  df = data.frame(evaluation=as.character(a.mt[,1]), id=as.character(a.mt[,2]), value=as.character(a.mt[,3]), method = sub('_.*', '', f), data = strsplit(f, '_')[[1]][2], test = strsplit(f, '_')[[1]][3], stringsAsFactors = F)
   df[,3] = sapply(seq(1,nrow(df)), function(ii) {
     if(df[ii, 1] == 'memory'){
       if (grepl('K',df[ii, 3])){
@@ -50,33 +51,12 @@ pd = do.call(rbind, eff)
 pd[,1] = as.character(pd[,1])
 pd[,2] = as.character(pd[,2])
 pd[,3] = as.numeric(pd[,3])
-pd[,5] = factor(as.character(pd[,5]), levels = c('hca_simu_cellprop','hca_tde', setdiff(unique(pd[,5]), c('hca_simu_cellprop','hca_tde'))))
 
 
-library(ggplot2)
-library(RColorBrewer)
-time = pd[pd[,1] == 'time', ]
-mem = pd[pd[,1] == 'memory', ]
+time = pd[pd[,1] == 'time(h)', ]
+mem = pd[pd[,1] == 'memory(GB)', ]
 saveRDS(time, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/efficiency/plot/time.rds')
 saveRDS(mem, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/efficiency/plot/memory.rds')
 
 write.csv(time, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/efficiency/plot/time.csv')
 write.csv(mem, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/efficiency/plot/memory.csv')
-
-
-p1 <- ggplot(time, aes(x = data, y = value, fill= method, color = method)) +
-  geom_boxplot(alpha = 0.1, scale = 'width') + 
-  # geom_jitter(size = 0.2, alpha = 1, width = 0.3) + 
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, color = 'black')) + 
-  ylab('time (hour)') + xlab('data')
-
-p2 <- ggplot(mem, aes(x = data, y = value, fill= method, color = method)) +
-  geom_boxplot(alpha = 0.1, scale = 'width') + 
-  # geom_jitter(size = 0.2, alpha = 1, width = 0.3) + 
-  theme_classic() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1, color = 'black')) + 
-  ylab('memory (GB)') + xlab('data')
-pdf('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/efficiency/plot/eff.pdf', width = 8, height = 3)
-gridExtra::grid.arrange(p1,p2, nrow = 1)
-dev.off()
