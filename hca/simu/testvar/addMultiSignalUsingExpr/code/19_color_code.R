f5d23bad9033e01aede6afb222eedbca9ebd4095
Wenpin Hou <wp.hou3@gmail.com>
2022-03-26 13:48:36 -0400

    19_color_code.R

--- hca/simu/testvar/addMultiSignalUsingExpr/code/19_color_code.R
+++ hca/simu/testvar/addMultiSignalUsingExpr/code/19_color_code.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(RColorBrewer)
 colv = c(brewer.pal(9,'Set1')[c(1:2)], brewer.pal(8, 'Dark2')[1:8])
 
-colv = c(colv, '80bf39', '9cd45d', 'bded87')
+colv = c(colv, '#80BF39', '#9CD45D', '#BDED87')
 
 allmet = c("Lamian",  "Lamian.chisq", "condiments", "limma",  "monocle2_trajTest",  "monocle2_trajTest.corr", "phenopath", 
  "tradeSeq_diffEndTest", "tradeSeq_earlyDETest", "tradeSeq_patternTest", 'phenopath500', 'phenopath100', 'phenopath300')
