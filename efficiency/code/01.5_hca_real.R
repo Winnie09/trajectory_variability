a3a602eed9060784ca20069b7d35cf352c202cdd
Wenpin Hou <wp.hou3@gmail.com>
2022-03-26 22:19:48 -0400

    01.5_hca_real.R

--- efficiency/code/01.5_hca_real.R
+++ efficiency/code/01.5_hca_real.R
@@ -15,3 +15,25 @@ for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/re
 do
 sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> Lamian.pm_hca_tde.txt
 done
+
+## condiments
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testvar/code/ | grep run37.sh.e  | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss > condiments_hca_xde.txt
+done
+
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testvar/code/ | grep run38.sh.e  | sed 's/.*.sh.e//g'`
+do
+sacct -j $i --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> condiments_hca_xde.txt
+done
+
+
+for i in `ls /home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/hca/real/testvar/code/ | grep run39.sh.e  | sed 's/.*.sh.e//g'`
+do
+sacct -j $i   --format=User,JobID,Jobname,partition,state,time,start,end,elapsed,MaxRss >> condiments_hca_xde.txt
+done
+
+
+
+
+
