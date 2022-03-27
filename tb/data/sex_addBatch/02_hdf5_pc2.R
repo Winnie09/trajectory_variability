38c736c41a2e207a9292c9d3dbb0bea6b5fdd8d6
Wenpin Hou <wp.hou3@gmail.com>
2022-03-27 11:17:26 -0400

    02_hdf5_pc2.R

---
+++ tb/data/sex_addBatch/02_hdf5_pc2.R
@@ -0,0 +1,8 @@
+source('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/h5func/saveh5.R')
+rdir = '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/data/sex_addBatch/'
+expr <- readRDS(paste0(rdir, 'expr.rds'))
+pseudotime <- readRDS(paste0(rdir, 'ptpc2.rds'))
+cellanno <- readRDS(paste0(rdir, 'cellanno.rds'))
+path <- paste0(rdir, 'exprpc2.h5')
+saveh5(expr2,pseudotime,cellanno,path)
+
