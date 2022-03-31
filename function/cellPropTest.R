459beb5f95f6484b1897b3b5b0618fd5f3ba0bf9
Wenpin Hou <wp.hou3@gmail.com>
2022-03-31 14:44:16 -0400

    add multi test

--- function/cellPropTest.R
+++ function/cellPropTest.R
@@ -1,4 +1,4 @@
-cellPropTest <- function(cellanno, pseudotime, design=NULL, ncores=detectCores(), test.type = 'variable') {
+cellPropTest <- function(cellanno, pseudotime, design=NULL, ncores=detectCores(), test.type = 'variable',  testvar = 2) {
   ptw <- cut(pseudotime,seq(min(pseudotime),max(pseudotime),length.out = 100),include.lowest = T)
   ptdat <- table(ptw,cellanno[match(names(pseudotime),cellanno[,1]),2])
   ptdat <- t(t(ptdat)/colSums(ptdat)) ## divided by rowsum (rowsum = 1). interval * samples. 
@@ -13,8 +13,10 @@ cellPropTest <- function(cellanno, pseudotime, design=NULL, ncores=detectCores()
   ptpt <- ptdat$pt
   names(ptpt) <- ptdat$cell
   ptcellanno <- data.frame(cell=ptdat$cell,sample=ptdat$s, stringsAsFactors = F)
-  res <- testpt(expr=ptexpr, cellanno=ptcellanno, pseudotime=ptpt, design=design, ncores=ncores, test.type = test.type, demean = FALSE, test.method = 'permutation', ncores.fit = 1,fix.all.zero=F)
+  res <- testpt(expr=ptexpr, cellanno=ptcellanno, pseudotime=ptpt, design=design,  testvar = testvar, ncores=ncores, test.type = test.type, demean = FALSE, test.method = 'permutation', ncores.fit = 1,fix.all.zero=F)
+  
   res$statistics <- res$statistics[1, 2:3]
   return(res)
 }
+
 
