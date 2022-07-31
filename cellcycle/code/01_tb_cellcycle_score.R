fc874b3935752584d18c0748631a1d65e972ad4f
whou10 <whou10@c720.rockfish.cluster>
2022-07-31 10:35:38 -0400

    re-calcualte cell cycle scores as mean, not use b-spline fitting

--- cellcycle/code/01_tb_cellcycle_score.R
+++ cellcycle/code/01_tb_cellcycle_score.R
@@ -1,14 +1,16 @@
 library(Seurat)
 pid = 2
-pt = readRDS(paste0('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/data/sex/ptpc',pid,'.rds'))
-expr = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/data/sex/expr.rds')
-cellanno = readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/data/sex/cellanno.rds')
-design <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/data/sex/design.rds') ## ## female = 1, male = 0
+pt = readRDS(paste0('tb/data/sex/ptpc',pid,'.rds'))
+expr = readRDS('tb/data/sex/expr.rds')
+cellanno = readRDS('tb/data/sex/cellanno.rds')
+design <- readRDS('tb/data/sex/design.rds') ## ## female = 1, male = 0
 
-cc.s = colSums(expr[intersect(cc.genes[[1]], rownames(expr)),])
-cc.g2m = colSums(expr[intersect(cc.genes[[2]], rownames(expr)),])
+cc.genes[['g1.genes']] <- c('CDC23',	'CDC25C',	'CDC6',	'CDK10',	'CDK2',	'CDK6',	'CDKN1C',	'E2F1',	'FOXO4',	'GFI1B',	'MAP3K11',	'PRUNE2',	'RB1',	'TAF1',	'TBRG4')
 
-bs.all = bs(pt)
+cc.s = colMeans(expr[intersect(cc.genes[[1]], rownames(expr)),])
+cc.g2m = colMeans(expr[intersect(cc.genes[[2]], rownames(expr)),])
+cc.g1 = colMeans(expr[intersect(cc.genes[[3]], rownames(expr)),])
+
 pdlist <- lapply(unique(cellanno[,2]), function(sample){
   ## subset the cells and pseudotime of this sample
   cell.tmp = cellanno[cellanno[,2] == sample, 1]
@@ -17,31 +19,17 @@ pdlist <- lapply(unique(cellanno[,2]), function(sample){
   ## cellcycle scores of s genes 
   cc.s.tmp = cc.s[names(pt.tmp)] 
   cc.g2m.tmp = cc.g2m[names(pt.tmp)]
+  cc.g1.tmp = cc.g1[names(pt.tmp)]
+
   d = data.frame(cc.s.sample = cc.s.tmp, 
                  cc.g2m.sample = cc.g2m.tmp,
+                 cc.g1.sample = cc.g1.tmp,
                  pt.sample = pt.tmp, 
+                 sample = sample, 
+                 sex = ifelse(design[sample, 2], 'female', 'male'),
                  stringsAsFactors = FALSE)
-  
-  ## cellcycle scores of s genes
-  fit = lm(cc.s.sample ~ bs.all[cell.tmp, ], data = d)
-  summary(fit)
-  bs.grid = cbind(1,bs.all[round(seq(1,nrow(bs.all),length.out = 1000)),])
-  cc.s.pred = (bs.grid %*% fit$coefficient)[,1]
-  pt.grid = pt[round(seq(1,nrow(bs.all),length.out = 1000))]
-  
-  ## cellcycle scores of g2m genes
-  fit = lm(cc.g2m.sample ~ bs.all[cell.tmp, ], data = d)
-  cc.g2m.pred = (bs.grid %*% fit$coefficient)[,1]
-  
-  ## return the dataframe containing all the scores
-  data.frame(cc.s.sample = cc.s.pred, 
-             cc.g2m.sample = cc.g2m.pred, 
-             pt.sample = pt.grid, 
-             sample = sample, 
-             sex = ifelse(design[sample, 2], 'female', 'male'))
-  #plot(pt.tmp, cc.tmp, col='grey', xlab = 'pt', ylab = 'cc')
-  #points(pt.grid, pred, col = 'darkgreen')
 })
 pd <- do.call(rbind, pdlist)
-saveRDS(pd, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/cellcycle/res/tb_cellcycle_score.rds')
+saveRDS(pd, 'cellcycle/res/tb_cellcycle_score.rds')
+
 
--- cellcycle/code/03_covid_mod_mi_cellcycle_score.R
+++ cellcycle/code/03_covid_mod_mi_cellcycle_score.R
@@ -1,17 +1,16 @@
 library(Seurat)
-expr <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/covid/Su_2020_Cell/data/saver_log2norm_sub.rds')
-pt <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/covid/Su_2020_Cell/data/tActivate_pseudotime.rds')
-#meta <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/covid/Su_2020_Cell/data/meta.rds')
-cellanno <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/covid/Su_2020_Cell/data/cellanno.rds')
-design <- readRDS('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/covid/Su_2020_Cell/data/design_numeric_Mod_Mi.rds') ## Mod = 0, Mi = 1
+expr <- readRDS('covid/Su_2020_Cell/data/saver_log2norm_sub.rds')
+pt <- readRDS('covid/Su_2020_Cell/data/tActivate_pseudotime.rds')
+#meta <- readRDS('covid/Su_2020_Cell/data/meta.rds')
+cellanno <- readRDS('covid/Su_2020_Cell/data/cellanno.rds')
+design <- readRDS('covid/Su_2020_Cell/data/design_numeric_Mod_Mi.rds') ## Mod = 0, Mi = 1
 
+cc.genes[['g1.genes']] <- c('CDC23',	'CDC25C',	'CDC6',	'CDK10',	'CDK2',	'CDK6',	'CDKN1C',	'E2F1',	'FOXO4',	'GFI1B',	'MAP3K11',	'PRUNE2',	'RB1',	'TAF1',	'TBRG4')
 
+cc.s = colMeans(expr[intersect(cc.genes[[1]], rownames(expr)),])
+cc.g2m = colMeans(expr[intersect(cc.genes[[2]], rownames(expr)),])
+cc.g1 = colMeans(expr[intersect(cc.genes[[3]], rownames(expr)),])
 
-cc.s = colSums(expr[intersect(cc.genes[[1]], rownames(expr)),])
-cc.g2m = colSums(expr[intersect(cc.genes[[2]], rownames(expr)),])
-
-library(splines)
-bs.all = bs(pt)
 pdlist <- lapply(unique(rownames(design)), function(sample){
   ## subset the cells and pseudotime of this sample
   cell.tmp = cellanno[cellanno[,2] == sample, 1]
@@ -20,31 +19,17 @@ pdlist <- lapply(unique(rownames(design)), function(sample){
   ## cellcycle scores of s genes 
   cc.s.tmp = cc.s[names(pt.tmp)] 
   cc.g2m.tmp = cc.g2m[names(pt.tmp)]
+  cc.g1.tmp = cc.g1[names(pt.tmp)]
+
   d = data.frame(cc.s.sample = cc.s.tmp, 
                  cc.g2m.sample = cc.g2m.tmp,
+                 cc.g1.sample = cc.g1.tmp,
                  pt.sample = pt.tmp, 
+                 sample = sample, 
+                 severity = ifelse(design[sample, 2], 'Mi', 'Mod'),
                  stringsAsFactors = FALSE)
-  
-  ## cellcycle scores of s genes
-  fit = lm(cc.s.sample ~ bs.all[cell.tmp, ], data = d)
-  summary(fit)
-  bs.grid = cbind(1,bs.all[round(seq(1,nrow(bs.all),length.out = 1000)),])
-  cc.s.pred = (bs.grid %*% fit$coefficient)[,1]
-  pt.grid = pt[round(seq(1,nrow(bs.all),length.out = 1000))]
-  
-  ## cellcycle scores of g2m genes
-  fit = lm(cc.g2m.sample ~ bs.all[cell.tmp, ], data = d)
-  cc.g2m.pred = (bs.grid %*% fit$coefficient)[,1]
-  
-  ## return the dataframe containing all the scores
-  a = data.frame(cc.s.sample = cc.s.pred, 
-             cc.g2m.sample = cc.g2m.pred, 
-             pt.sample = pt.grid, 
-             sample = sample, 
-             severity = ifelse(design[sample, 2], 'Mi', 'Mod'))
-  #plot(pt.tmp, cc.tmp, col='grey', xlab = 'pt', ylab = 'cc')
-  #points(pt.grid, pred, col = 'darkgreen')
 })
 pd <- do.call(rbind, pdlist)
-saveRDS(pd, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/cellcycle/res/covid_Mod_Mi_cellcycle_score.rds')
+saveRDS(pd, 'cellcycle/res/covid_Mod_Mi_cellcycle_score.rds')
+
 
