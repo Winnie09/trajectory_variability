9c6a1188de6b3d5ec50c552f5db471574688c5e8
whou10 <whou10@c720.rockfish.cluster>
2022-08-07 17:34:07 -0400

    add facet arguments, xlab, ylab

--- function/plotClusterDiff.R
+++ function/plotClusterDiff.R
@@ -2,6 +2,9 @@ plotClusterDiff <- function(testobj,
                             gene = names(testobj$cluster),
                             cluster = testobj[['cluster']],
                             each = FALSE,
+                            facet_variable = 'gene',
+                            facet_scales = 'free',
+                            facet_nrow = 3,
                             sep = '',
                             reverse = F){
   if ('covariateGroupDiff' %in% names(testobj)){
@@ -13,13 +16,15 @@ plotClusterDiff <- function(testobj,
   library(ggplot2)
   library(RColorBrewer)
   library(reshape2)
-if (each){
+if (each && toupper(facet_variable) == 'GENE'){
   pd <- melt(fit)
   colnames(pd) <- c('gene', 'pseudotime', 'covariateGroupDiff')
   pd[,1] <- sub(sep, '', pd[,1])
   p <- ggplot(data = pd) + geom_line(aes(x = pseudotime, y = covariateGroupDiff))+
-    theme_classic()+
-    facet_wrap(~gene)
+    theme_classic()
+  
+    p <- p + facet_wrap(~gene, scales = facet_scales, nrow = facet_nrow)
+    
 }  else {
   clu = cluster[gene]
   tmp <- sapply(sort(unique(clu)), function(i){
@@ -32,13 +37,20 @@ if (each){
   pd$cluster <- factor(pd$cluster)
   p <- ggplot(data = pd) + geom_line(aes(x = pseudotime, y = covariateGroupDiff, color = cluster))+
     theme_classic() + scale_x_continuous(breaks=c(min(pd$pseudotime),max(pd$pseudotime)))
+  
+  if (each && toupper(facet_variable) == 'CLUSTER'){
+    p <- p + facet_wrap(~cluster, scales = facet_scales, nrow = facet_nrow)
+  }
+  
   if (length(unique(pd$cluster)) < 8){
     p <- p + scale_color_brewer(palette = 'Dark2')
   } else {
     p <- p + scale_color_manual(values = colorRampPalette(brewer.pal(8,'Dark2'))(length(unique(pd$cluster))))
   }
 }
-   print(p)
+  p <- p + xlab('Pseudotime')  + ylab('Covariate group difference')
+  print(p)
 }
+
 
 
