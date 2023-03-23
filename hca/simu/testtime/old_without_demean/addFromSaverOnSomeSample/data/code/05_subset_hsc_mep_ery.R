fd239ed47b6df8a60669c1984d416a01e1bf502e
whou10 <whou10@c717.rockfish.cluster>
2023-03-22 22:42:41 -0400

    ‘date’

--- hca/simu/testtime/old_without_demean/addFromSaverOnSomeSample/data/code/05_subset_hsc_mep_ery.R
+++
@@ -1,9 +0,0 @@
-setwd('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/')
-rdir <- './testvar/data/plain/'
-expr <- readRDS('./hca/data/HCA/proc/matrix/saver.rds')
-ct <- readRDS('./hca/data/HCA/proc/ct/sc.rds')
-id <- which(ct %in% c('HSC','MEP','Ery'))
-expr <- expr[,id]
-expr <- expr[rowMeans(expr > 0.1) > 0.1,] ## [1:9070, 1:13269] 
-saveRDS(expr, '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/testtime/data/data/null/hsc_mep_ery.rds')
-
