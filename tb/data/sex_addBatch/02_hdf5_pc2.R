4027974d5b510979d5f23942a85106303733b668
whou10 <whou10@c719.rockfish.cluster>
2023-03-23 11:41:54 -0400

    ‘date’

--- tb/data/sex_addBatch/02_hdf5_pc2.R
+++
@@ -1,8 +0,0 @@
-source('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/h5func/saveh5.R')
-rdir = '/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/tb/data/sex_addBatch/'
-expr <- readRDS(paste0(rdir, 'expr.rds'))
-pseudotime <- readRDS(paste0(rdir, 'ptpc2.rds'))
-cellanno <- readRDS(paste0(rdir, 'cellanno.rds'))
-path <- paste0(rdir, 'exprpc2.h5')
-saveh5(expr,pseudotime,cellanno,path)
-
