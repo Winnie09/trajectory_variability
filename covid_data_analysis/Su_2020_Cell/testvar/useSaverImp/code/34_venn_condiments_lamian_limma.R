7c09a97a9b861d7b2f7a45c1ebf73ce9fb071115
whou10 <wp.hou3@gmail.com>
2023-07-28 22:04:02 -0400

    34_venn_condiments_lamian_limma.R

--- covid_data_analysis/Su_2020_Cell/testvar/useSaverImp/code/34_venn_condiments_lamian_limma.R
+++ covid_data_analysis/Su_2020_Cell/testvar/useSaverImp/code/34_venn_condiments_lamian_limma.R
@@ -1,10 +1,12 @@
 library(here)
 setwd(here())
-lamian <- readRDS(paste0('covid/Su_2020_Cell/testvar/useSaverImp/result/EM_pm/Mod_Mi/numeric_res.rds'))[[1]]
+
+
+lamian <- readRDS(paste0('covid_data_analysis/Su_2020_Cell/testvar/useSaverImp/result/EM_pm/Mod_Mi/numeric_res.rds'))[[1]]
 lamian <- rownames(lamian)[lamian[,'fdr.overall'] < 0.05]
-limma <- readRDS('covid/Su_2020_Cell/testvar/useSaverImp/result/limma/Mod_Mi.rds')
+limma <- readRDS('covid_data_analysis/Su_2020_Cell/testvar/useSaverImp/result/limma/Mod_Mi.rds')
 limma <- rownames(limma)[limma$adj.P.Val < 0.05]
-cond <- readRDS('covid/Su_2020_Cell/testvar/useSaverImp/result/condiments/Mod_Mi/cond_gene_res.rds')
+cond <- readRDS('covid_data_analysis/Su_2020_Cell/testvar/useSaverImp/result/condiments/Mod_Mi/cond_gene_res.rds')
 cond[,4] <- p.adjust(cond[,3])
 colnames(cond)[4] <- 'FDR'
 cond <- rownames(cond)[cond[,4]<0.05]
@@ -12,10 +14,16 @@ cond <- rownames(cond)[cond[,4]<0.05]
 library(VennDiagram)
 library(RColorBrewer)
 myCol <- brewer.pal(3, "Pastel2")
+
+
+pd = list(cond, lamian,limma)
+str(pd)
+saveRDS(pd, 'covid_data_analysis/Su_2020_Cell/testvar/useSaverImp/plot/perf/venn_condiments_lamian_limma_5H.rds')
+
 venn.diagram(
   x = list(cond, lamian,limma),
   category.names = c("condiments", "Lamian" , "Limma"),
-  filename = 'covid/Su_2020_Cell/testvar/useSaverImp/plot/perf/venn_condiments_lamian_limma.png',
+  filename = 'covid_data_analysis/Su_2020_Cell/testvar/useSaverImp/plot/perf/venn_condiments_lamian_limma.png',
   output=TRUE,
   
   # Output features
@@ -44,6 +52,7 @@ venn.diagram(
   cat.fontfamily = "sans",
   rotation = 1
 )
+
 
 
 
