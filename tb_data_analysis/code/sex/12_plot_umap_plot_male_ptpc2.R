4fe99b64406a93aad25c0d597b784f0962b3d89d
whou10 <wp.hou3@gmail.com>
2023-07-29 07:48:26 -0400

    update path

--- tb_data_analysis/code/sex/12_plot_umap_plot_male_ptpc2.R
+++ tb_data_analysis/code/sex/12_plot_umap_plot_male_ptpc2.R
@@ -1,10 +1,10 @@
 library(here)
 setwd(here())
 #setwd('/Users/wenpinhou/Dropbox/trajectory_variability/')
-a = read.csv('tb/data/raw/mRNA_NAM_PCs.csv')
+a = read.csv('tb_data_analysis/data/raw/mRNA_NAM_PCs.csv')
 # b = read.csv('tb/data/raw/CCA_NAM_PCs.csv')
-c = readRDS('tb/data/sex/cellanno.rds')
-design = readRDS('tb/data/sex/design.rds')
+c = readRDS('tb_data_analysis/data/sex/cellanno.rds')
+design = readRDS('tb_data_analysis/data/sex/design.rds')
 # nampc1 = b[,4]
 # names(nampc1) = b[,1]
 nampc2 = a[,4]
@@ -18,7 +18,7 @@ names(sex) = sub('_.*', '', rownames(design))
 sex = sex[sub('_.*', '', c[cell,2])]
 sex = ifelse(sex == 1, 'female', 'male')
 
-u = readRDS('tb/data/sex/umap.rds')
+u = readRDS('tb_data_analysis/data/sex/umap.rds')
 rownames(u) = sub('.*:', '', rownames(u))
 u = u[cell, ]
 
@@ -30,12 +30,13 @@ source('/home-4/whou10@jhu.edu/scratch/Wenpin/resource/ggplot_theme.R')
 # source('resource/ggplot_theme.R')
 theme_set(.new_theme)
 
-pdir = 'tb/plot/sex/'
-pt = readRDS('tb/data/sex/ptpc2.rds')
+pdir = 'tb_data_analysis/plot/sex/'
+pt = readRDS('tb_data_analysis/data/sex/ptpc2.rds')
 names(pt) = sub('.*:', '', names(pt))
 
 pdf(paste0(pdir, 'umap_plot_male_ptpc2.pdf'), width = 2.4, height = 1.6)
 df = data.frame(nampc1 = u[,1], nampc2 = u[,2], sex = sex, pseudotime = pt[cell], stringsAsFactors = FALSE)
+saveRDS(df, paste0(pdir, 'umap_plot_male_ptpc2_6A.rds'))
 ggplot(data = df, aes(x = nampc1, y = nampc2, color = pseudotime, alpha = 0.5)) + geom_scattermore(size = 0.001, stroke = 0) +
   #scale_colour_manual(values = c('orange', 'royalblue')) +
   #guides(color = guide_legend(override.aes = list(size = 3))) +
@@ -43,5 +44,6 @@ ggplot(data = df, aes(x = nampc1, y = nampc2, color = pseudotime, alpha = 0.5)) 
   #labs(fill = 'pseudotime')+
   xlab('UMAP1') + ylab('UMAP2')
 dev.off()
+
 
 
